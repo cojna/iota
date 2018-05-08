0708094baee2692ffd287b91824c70a5e3ef590e
cojna <cojna@users.noreply.github.com>
2018-05-08 12:49:46 +0900

    IntMod Vector instance

---
+++ src/Data/IntMod/Vector.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies          #-}
+
+module Data.IntMod.Vector where
+
+import           Control.Monad
+import           Data.IntMod
+import qualified Data.Vector.Generic         as G
+import qualified Data.Vector.Generic.Mutable as GM
+import qualified Data.Vector.Unboxed         as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+
+newtype instance UM.MVector s IntMod = MV_IntMod (UM.MVector s Int)
+newtype instance U.Vector IntMod = V_IntMod (U.Vector Int)
+
+instance U.Unbox IntMod
+
+instance GM.MVector UM.MVector IntMod where
+    {-# INLINE basicLength #-}
+    {-# INLINE basicUnsafeSlice #-}
+    {-# INLINE basicOverlaps #-}
+    {-# INLINE basicUnsafeNew #-}
+    {-# INLINE basicInitialize #-}
+    {-# INLINE basicUnsafeReplicate #-}
+    {-# INLINE basicUnsafeRead #-}
+    {-# INLINE basicUnsafeWrite #-}
+    {-# INLINE basicClear #-}
+    {-# INLINE basicSet #-}
+    {-# INLINE basicUnsafeCopy #-}
+    {-# INLINE basicUnsafeGrow #-}
+    basicLength (MV_IntMod v) = GM.basicLength v
+    basicUnsafeSlice i n (MV_IntMod v) = MV_IntMod $ GM.basicUnsafeSlice i n v
+    basicOverlaps (MV_IntMod v1) (MV_IntMod v2) = GM.basicOverlaps v1 v2
+    basicUnsafeNew n = MV_IntMod `liftM` GM.basicUnsafeNew n
+    basicInitialize (MV_IntMod v) = GM.basicInitialize v
+    basicUnsafeReplicate n x = MV_IntMod `liftM` GM.basicUnsafeReplicate n (unMod x)
+    basicUnsafeRead (MV_IntMod v) i = Mod `liftM` GM.basicUnsafeRead v i
+    basicUnsafeWrite (MV_IntMod v) i x = GM.basicUnsafeWrite v i (unMod x)
+    basicClear (MV_IntMod v) = GM.basicClear v
+    basicSet (MV_IntMod v) x = GM.basicSet v (unMod x)
+    basicUnsafeCopy (MV_IntMod v1) (MV_IntMod v2) = GM.basicUnsafeCopy v1 v2
+    basicUnsafeMove (MV_IntMod v1) (MV_IntMod v2) = GM.basicUnsafeMove v1 v2
+    basicUnsafeGrow (MV_IntMod v) n = MV_IntMod `liftM` GM.basicUnsafeGrow v n
+
+instance G.Vector U.Vector IntMod where
+    {-# INLINE basicUnsafeFreeze #-}
+    {-# INLINE basicUnsafeThaw #-}
+    {-# INLINE basicLength #-}
+    {-# INLINE basicUnsafeSlice #-}
+    {-# INLINE basicUnsafeIndexM #-}
+    {-# INLINE elemseq #-}
+    basicUnsafeFreeze (MV_IntMod v) = V_IntMod `liftM` G.basicUnsafeFreeze v
+    basicUnsafeThaw (V_IntMod v) = MV_IntMod `liftM` G.basicUnsafeThaw v
+    basicLength (V_IntMod v) = G.basicLength v
+    basicUnsafeSlice i n (V_IntMod v) = V_IntMod $ G.basicUnsafeSlice i n v
+    basicUnsafeIndexM (V_IntMod v) i = Mod `liftM` G.basicUnsafeIndexM v i
+    basicUnsafeCopy (MV_IntMod mv) (V_IntMod v) = G.basicUnsafeCopy mv v
+    elemseq _ = seq
