2acc6adc5f8c42bcabc74abf4e0758c7b4ee99a4
cojna <cojna@users.noreply.github.com>
2018-05-23 20:10:01 +0900

    Matrix3x3 test

---
+++ test/Data/Matrix3x3Spec.hs
@@ -0,0 +1,18 @@
+module Data.Matrix3x3Spec where
+
+import           Data.Matrix3x3
+import           Data.Proxy
+import           Test.Hspec
+import           Test.Hspec.QuickCheck     (prop)
+import           Test.QuickCheck
+import           Test.QuickCheck.Arbitrary
+import           Util.NumProp
+
+spec :: Spec
+spec = numSpec (Proxy :: Proxy (Mat3x3 Int))
+
+instance Arbitrary a => Arbitrary (Mat3x3 a) where
+    arbitrary = M33
+        <$> arbitrary <*> arbitrary <*> arbitrary
+        <*> arbitrary <*> arbitrary <*> arbitrary
+        <*> arbitrary <*> arbitrary <*> arbitrary
