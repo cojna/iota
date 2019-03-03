7752814d07480d017d266ddbfe1c126c1df30413
cojna <cojna@users.noreply.github.com>
2019-03-04 00:05:15 +0900

    Improve UnionFind

--- src/Data/UnionFind/Vector.hs
+++ src/Data/UnionFind/Vector.hs
@@ -5,26 +5,20 @@ module Data.UnionFind.Vector where
 
 import           Control.Monad
 import           Control.Monad.Primitive
+import           Data.Function
 import qualified Data.Vector.Unboxed         as U
 import qualified Data.Vector.Unboxed.Mutable as UM
 
-data UnionFind m = UF
-    { parent :: UM.MVector m Int
-    , rank   :: UM.MVector m Int
-    }
-
-nothing :: Int
-nothing = -1
-{-# INLINE nothing #-}
+newtype UnionFind m = UF { parent :: UM.MVector m Int }
 
 newUnionFind :: PrimMonad m => Int -> m (UnionFind (PrimState m))
-newUnionFind n = UF `liftM` UM.replicate n nothing `ap` UM.replicate n 0
+newUnionFind n = UF <$> UM.replicate n (-1)
 {-# INLINE newUnionFind #-}
 
 findM :: PrimMonad m => UnionFind (PrimState m) -> Int -> m Int
 findM uf@UF{..} x = do
     px <- UM.unsafeRead parent x
-    if px == nothing
+    if px < 0
     then return x
     else do
         ppx <- findM uf px
@@ -32,19 +26,31 @@ findM uf@UF{..} x = do
         return ppx
 {-# INLINE findM #-}
 
-uniteM :: PrimMonad m => UnionFind (PrimState m) -> Int -> Int -> m ()
-uniteM uf@UF{..} x y= do
+sizeM :: PrimMonad m => UnionFind (PrimState m) -> Int -> m Int
+sizeM uf@UF{..} = fix $ \loop x -> do
+    px <- UM.unsafeRead parent x
+    if px < 0
+    then return $! negate px
+    else loop px
+{-# INLINE sizeM #-}
+
+uniteM :: PrimMonad m => UnionFind (PrimState m) -> Int -> Int -> m Bool
+uniteM uf@UF{..} x y = do
     px <- findM uf x
     py <- findM uf y
-    when (px /= py) $ do
-        rx <- UM.unsafeRead rank px
-        ry <- UM.unsafeRead rank py
-        case compare rx ry of
-            LT -> UM.unsafeWrite parent px py
-            GT -> UM.unsafeWrite parent py px
-            EQ -> do
-                UM.unsafeWrite parent px py
-                UM.unsafeWrite rank py (ry + 1)
+    if px == py
+    then return False
+    else do
+        rx <- UM.unsafeRead parent px
+        ry <- UM.unsafeRead parent py
+        if rx < ry
+        then do
+            UM.unsafeModify parent (+ry) px
+            UM.unsafeWrite parent py px
+        else do
+            UM.unsafeModify parent (+rx) py
+            UM.unsafeWrite parent px py
+        return True
 {-# INLINE uniteM #-}
 
 equivM :: PrimMonad m => UnionFind (PrimState m) -> Int -> Int -> m Bool
@@ -53,5 +59,5 @@ equivM uf x y = (==) `liftM` findM uf x `ap` findM uf y
 
 -- | O(n)
 countGroupM :: PrimMonad m => UnionFind (PrimState m) -> m Int
-countGroupM UF{..} = U.length . U.filter (==nothing) <$> U.unsafeFreeze parent
+countGroupM UF{..} = U.length . U.filter (<0) <$> U.unsafeFreeze parent
 {-# INLINE countGroupM #-}
