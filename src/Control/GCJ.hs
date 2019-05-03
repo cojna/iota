53c61143d4abad79d9fc19c00ec68b50182e1ef6
cojna <cojna@users.noreply.github.com>
2019-05-03 19:26:30 +0900

    GCJ interactive (#24)

--- src/Control/GCJ.hs
+++ src/Control/GCJ.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE CPP #-}
+
 module Control.GCJ where
 
-import Control.Monad
+import           Control.Monad
+import qualified System.IO      as IO
+import           System.Process
 
 runGCJ :: IO () -> IO ()
 runGCJ main_ = do
@@ -8,3 +12,18 @@ runGCJ main_ = do
     forM_ [1..t] $ \i -> do
         putStr $ "Case #" ++ shows i ": "
         main_
+
+runInteractive :: (IO.Handle -> IO.Handle -> IO ()) -> IO ()
+runInteractive action = do
+#ifdef DEBUG
+    (Just hout, Just hin, _, _) <- createProcess
+        (shell "python3 testing_tool.py 0")
+            { std_in = CreatePipe, std_out = CreatePipe}
+#else
+    let hin = IO.stdin
+    let hout = IO.stdout
+#endif
+    t:_ <- map read.words <$> IO.hGetLine hin :: IO [Int]
+    forM_ [1..t] $ \i -> do
+        IO.hPutStrLn IO.stderr $ "Case #" ++ shows i ":"
+        action hin hout
