32142fd1e76990b88dfd62265cdfda65d6eb37cc
cojna <cojna@users.noreply.github.com>
2019-05-05 18:41:20 +0900

    Remove redundant imports

--- src/Data/Graph/Grid.hs
+++ src/Data/Graph/Grid.hs
@@ -1,10 +1,7 @@
 module Data.Graph.Grid where
 
 import qualified Data.ByteString.Char8    as C
-import qualified Data.ByteString.Internal as B
-import qualified Data.Vector.Storable     as SV
 import qualified Data.Vector.Unboxed      as U
-import           Data.Word
 
 inGrid :: Int -> Int -> Int -> Int -> Bool
 inGrid h w x y = 0 <= x && x < h && 0 <= y && y < w
