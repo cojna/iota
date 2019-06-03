7fae37f5b2bb467573ae98d34f5dd5fe84d26f9b
cojna <cojna@users.noreply.github.com>
2019-06-03 12:05:18 +0900

    radixSort Int / (Int, Int) / (Int, Int, Int)

--- test/Data/Vector/SortSpec.hs
+++ test/Data/Vector/SortSpec.hs
@@ -3,6 +3,7 @@
 module Data.Vector.SortSpec where
 
 import qualified Data.List                 as L
+import           Data.Proxy
 import           Data.Vector.Sort
 import qualified Data.Vector.Unboxed       as U
 import           Data.Word
@@ -10,6 +11,8 @@ import           Test.Hspec
 import           Test.Hspec.QuickCheck     (prop)
 import           Test.QuickCheck
 import           Test.QuickCheck.Arbitrary
+--
+import           Data.Word64
 
 spec :: Spec
 spec = do
@@ -19,6 +22,18 @@ spec = do
         prop "naive" prop_radixSort32Naive
     describe "radixSort64" $
         prop "naive" prop_radixSort64Naive
+    describe "radixSort/Int" $
+        prop "naive" $ prop_radixSortNaive (Proxy :: Proxy Int)
+    describe "radixSort/(Int, Int)" $
+        prop "naive" $ prop_radixSortNaive (Proxy :: Proxy (Int, Int))
+    describe "radixSort/(Int, Int, Int)" $
+        prop "naive" $ prop_radixSortNaive (Proxy :: Proxy (Int, Int, Int))
+    describe "radixSortNonNegative/Int" $
+        prop "naive" $ prop_radixSortNonNegativeNaiveInt
+    describe "radixSortNonNegative/(Int, Int)" $
+        prop "naive" $ prop_radixSortNonNegativeNaiveIntInt
+    describe "radixSortNonNegative/(Int, Int, Int)" $
+        prop "naive" $ prop_radixSortNonNegativeNaiveIntIntInt
 
 prop_bucketSortNaive :: [NonNegative Int] -> Bool
 prop_bucketSortNaive (map getNonNegative -> xs)
@@ -34,3 +49,23 @@ prop_radixSort32Naive xs
 prop_radixSort64Naive :: [Word64] -> Bool
 prop_radixSort64Naive xs
     = radixSort64 (U.fromList xs) == U.fromList (L.sort xs)
+
+prop_radixSortNaive
+    :: (Ord a, U.Unbox a, Word64Encode a) => Proxy a -> [a] -> Bool
+prop_radixSortNaive _ xs
+    = radixSort (U.fromList xs) == U.fromList (L.sort xs)
+
+prop_radixSortNonNegativeNaiveInt :: [NonNegative Int] -> Bool
+prop_radixSortNonNegativeNaiveInt (map getNonNegative -> xs)
+    = radixSort (U.fromList xs) == U.fromList (L.sort xs)
+
+prop_radixSortNonNegativeNaiveIntInt :: [(NonNegative Int, NonNegative Int)] -> Bool
+prop_radixSortNonNegativeNaiveIntInt
+    (map (\(x, y) -> (getNonNegative x, getNonNegative y)) -> xs)
+    = radixSort (U.fromList xs) == U.fromList (L.sort xs)
+
+prop_radixSortNonNegativeNaiveIntIntInt :: [(NonNegative Int, NonNegative Int, NonNegative Int)] -> Bool
+prop_radixSortNonNegativeNaiveIntIntInt
+    (map (\(x, y, z) -> (getNonNegative x, getNonNegative y, getNonNegative z)) -> xs)
+    = radixSort (U.fromList xs) == U.fromList (L.sort xs)
+
