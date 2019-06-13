51779a5b71d32c7fa31510e1dbb08e04cdc33b9c
cojna <cojna@users.noreply.github.com>
2019-06-14 00:54:57 +0900

    Prelude for tests

---
+++ test/Test/Prelude.hs
@@ -0,0 +1,11 @@
+module Test.Prelude
+    ( module Test.Hspec
+    , module Test.Hspec.QuickCheck
+    , module Test.QuickCheck
+    , module Test.QuickCheck.Arbitrary
+    ) where
+
+import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
+import           Test.QuickCheck.Arbitrary
