62645515194b621d48c0cfcc0d26d71c56f1f14c
cojna <cojna@users.noreply.github.com>
2019-06-15 01:24:40 +0900

    Fix logMod

--- src/Math/Modulus.hs
+++ src/Math/Modulus.hs
@@ -31,20 +31,20 @@ recipMod x m = go x m 1 0
 -- |
 -- Baby-step Giant-step
 -- @a^x = b (mod p)@　p is prime
--- @O(sqrt P * P)@
+-- @O(sqrt P * log P)@
 logMod :: Int -> Int -> Int -> Maybe Int
 logMod a b p = go 0 b
   where
     !sqrtP = ceiling . sqrt $ fromIntegral p
-    !g = powMod a sqrtP p
+    !g = powMod a (-sqrtP) p
     babyStep x = a * x `rem` p
     giantStep x = g * x `rem` p
 
     table :: IM.IntMap Int
-    !table = IM.fromList $ zip (L.iterate babyStep 1) [0..sqrtP]
+    !table = IM.fromList $ zip (iterate babyStep 1) [0..sqrtP]
 
     go !i !x
-        | i <= sqrtP = case IM.lookup x table of
-            Just j -> Just $! i + j
-            Nothing -> go (i + sqrtP) $ giantStep x
+        | i < sqrtP = case IM.lookup x table of
+            Just j -> Just $! i * sqrtP + j
+            Nothing -> go (i + 1) $ giantStep x
         | otherwise = Nothing
