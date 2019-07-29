8452d94316aa51ebee5de8f88f19ca0f77a7a010
cojna <cojna@users.noreply.github.com>
2019-07-30 01:39:16 +0900

    Z-Algorithm (#37)
    * Z-Algorithm
    
    * Add examples
    
    * Add INLINE pragmas
    
    * Support empty case
    
    * Add specs
    
    * Use ByteStringOf

---
+++ test/Data/ByteString/ZAlgorithmSpec.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE BangPatterns      #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns      #-}
+
+module Data.ByteString.ZAlgorithmSpec (main, spec) where
+
+import qualified Data.ByteString            as B
+import qualified Data.ByteString.Char8      as C
+import qualified Data.ByteString.Unsafe     as B
+import           Data.ByteString.ZAlgorithm
+import qualified Data.Vector.Unboxed        as U
+import           Test.Prelude
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+    describe "z-algorithm" $ do
+        prop "naive" prop_naive
+        it "zAlgorithm \"ababab\" = [6,0,4,0,2,0]" $ do
+            zAlgorithm "ababab" `shouldBe` U.fromList [6,0,4,0,2,0]
+        it "zAlgorithm \"abc$xabcxx\" = [10,0,0,0,0,3,0,0,0,0]" $ do
+            zAlgorithm "abc$xabcxx" `shouldBe` U.fromList [10,0,0,0,0,3,0,0,0,0]
+
+prop_naive :: ByteStringOf "ab" -> Bool
+prop_naive (getByteStringOf -> s)
+    = zAlgorithm s == naiveZAlgorithm s
+
+naiveZAlgorithm :: B.ByteString -> U.Vector Int
+naiveZAlgorithm bs = U.generate n (\i -> naiveLCP bs $ B.drop i bs)
+  where
+    !n = B.length bs
+
+naiveLCP :: B.ByteString -> B.ByteString -> Int
+naiveLCP xs ys = go 0
+  where
+    !n = B.length xs `min` B.length ys
+    go !i
+        | i < n, B.unsafeIndex xs i == B.unsafeIndex ys i = go (i + 1)
+        | otherwise = i
