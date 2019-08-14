ad9a4c115afda9e821cf5553c1f1ea86cc081231
cojna <cojna@users.noreply.github.com>
2019-08-15 00:57:21 +0900

    Bellman Ford

---
+++ src/Data/Graph/BellmanFord.hs
@@ -0,0 +1,37 @@
+module Data.Graph.BellmanFord where
+
+import           Control.Monad
+import qualified Data.Vector.Unboxed         as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+
+type Vertex = Int
+
+-- | Bellman-Ford O(VE)
+--
+-- dist[n] == maxBound iff v is unreachable
+--
+-- dist[v] == minBound iff v in negative cycle
+bellmanFord :: Int -> Vertex -> U.Vector (Vertex, Vertex, Int) -> U.Vector Int
+bellmanFord n root edges = U.create $ do
+    dist <- UM.replicate n maxBound
+    UM.write dist root 0
+    U.replicateM (n - 1) $ do
+        U.forM_ edges $ \(src, dst, cost) -> do
+            dv <- UM.unsafeRead dist src
+            du <- UM.unsafeRead dist dst
+            when (dv + cost < du && dv /= maxBound) $ do
+                UM.unsafeWrite dist dst $ dv + cost
+
+    U.forM_ edges $ \(src, dst, cost) -> do
+        dv <- UM.unsafeRead dist src
+        du <- UM.unsafeRead dist dst
+        when (dv + cost < du && dv /= maxBound) $ do
+            UM.unsafeWrite dist dst minBound
+
+    U.replicateM (n - 1) $ do
+        U.forM_ edges $ \(src, dst, _) -> do
+            dv <- UM.unsafeRead dist src
+            when (dv == minBound) $ do
+                UM.unsafeWrite dist dst minBound
+
+    return dist
