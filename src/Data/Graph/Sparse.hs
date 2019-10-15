e0949cf24e38cdba07a839f53c8991465d23baba
cojna <cojna@users.noreply.github.com>
2019-10-15 21:39:47 +0900

    Compressed Sparse Row Graph

--- src/Data/Graph/Sparse.hs
+++ src/Data/Graph/Sparse.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE LambdaCase      #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase, RecordWildCards #-}
 
 module Data.Graph.Sparse where
 
@@ -10,70 +9,77 @@ import           Data.Tuple
 import qualified Data.Vector                 as V
 import qualified Data.Vector.Unboxed         as U
 import qualified Data.Vector.Unboxed.Mutable as UM
---
-import           Data.VecQueue
 
 type Vertex = Int
 type Edge = (Vertex, Vertex)
-type Graph = U.Vector Int
+type EdgeWith w = (Vertex, Vertex, w)
+data SparseGraph w = CSR
+    { numVerticesCSR :: !Int
+    , numEdgesCSR    :: !Int
+    , offsetCSR      :: !(U.Vector Int)
+    , adjacentCSR    :: !(U.Vector Vertex)
+    , edgeCtxCSR     :: !(U.Vector w)
+    }
 
-buildDirectedGraph :: Int -> U.Vector Edge -> Graph
-buildDirectedGraph n edges = U.create $ do
-    offset <- U.unsafeThaw
-        . U.scanl' (+) (n + 1)
-        . U.unsafeAccumulate (+) (U.replicate n 0)
-        . U.map (flip (,) 1)
-        . fst
-        $ U.unzip edges
-    gr <- UM.replicate (UM.length offset + U.length edges) 0
-    U.forM_ (U.generate (n + 1) id) $ \i ->
-        UM.unsafeRead offset i >>= UM.unsafeWrite gr i
+buildDirectedGraph
+    :: Int -> U.Vector Edge -> SparseGraph ()
+buildDirectedGraph numVerticesCSR edges = runST $ do
+    let numEdgesCSR = U.length edges
+    let offsetCSR = U.scanl' (+) 0
+            . U.unsafeAccumulate (+) (U.replicate numVerticesCSR 0)
+            . U.map (flip (,) 1)
+            . fst
+            $ U.unzip edges
+    moffset <- U.thaw offsetCSR
+    madj <- UM.new numEdgesCSR
     U.forM_ edges $ \(src, dst) -> do
-        pos <- UM.unsafeRead offset src
-        UM.unsafeWrite offset src (pos + 1)
-        UM.unsafeWrite gr pos dst
-    return gr
+        pos <- UM.unsafeRead moffset src
+        UM.unsafeWrite moffset src (pos + 1)
+        UM.unsafeWrite madj pos dst
+    adjacentCSR <- U.unsafeFreeze madj
+    return CSR{edgeCtxCSR = U.replicate numEdgesCSR (), ..}
 
-buildUndirectedGraph :: Int -> U.Vector Edge -> Graph
+buildUndirectedGraph :: Int -> U.Vector Edge -> SparseGraph ()
 buildUndirectedGraph n edges
     = buildDirectedGraph n (edges U.++ U.map swap edges)
 
-numVertices :: Graph -> Int
-numVertices gr = U.head gr - 1
-
-numEdges :: Graph -> Int
-numEdges gr = U.length gr - U.head gr
-
-vertices :: Graph -> U.Vector Vertex
-vertices = flip U.generate id . numVertices
-
-adjacent :: Graph -> Int -> U.Vector Vertex
-adjacent gr v = U.unsafeSlice offset len gr
-  where
-    offset = U.unsafeIndex gr v
-    len = U.unsafeIndex gr (v + 1) - offset
-
-topSort :: Graph -> Maybe (U.Vector Int)
-topSort gr = runST $ do
-    let n = numVertices gr
-    q <- withCapacityQ n
-    let inDegree = U.unsafeAccumulate (+) (U.replicate n (0 :: Int))
+buildDirectedGraphW :: (U.Unbox w)
+    => Int -> U.Vector (EdgeWith w) -> SparseGraph w
+buildDirectedGraphW numVerticesCSR edges = runST $ do
+    let numEdgesCSR = U.length edges
+    let offsetCSR = U.scanl' (+) 0
+            . U.unsafeAccumulate (+) (U.replicate numVerticesCSR 0)
             . U.map (flip (,) 1)
-            $ U.drop (n + 1) gr
-    U.mapM_ (flip enqueue q . fst)
-        . U.filter ((== 0) . snd)
-        $ U.indexed inDegree
-    inDeg <- U.unsafeThaw inDegree
-    fix $ \loop -> do
-        dequeue q >>= \case
-            Just v -> do
-                U.forM_ (adjacent gr v) $ \u -> do
-                    UM.unsafeRead inDeg u >>= \case
-                        1 -> enqueue u q
-                        i -> UM.unsafeWrite inDeg u (i - 1)
-                loop
-            Nothing -> return()
-    res <- freezeQueueData q
-    if U.length res == n
-    then return $ Just res
-    else return Nothing
+            . (\(x, _, _) -> x)
+            $ U.unzip3 edges
+    moffset <- U.thaw offsetCSR
+    madj <- UM.new numEdgesCSR
+    mectx <- UM.new numEdgesCSR
+    U.forM_ edges $ \(src, dst, w) -> do
+        pos <- UM.unsafeRead moffset src
+        UM.unsafeWrite moffset src (pos + 1)
+        UM.unsafeWrite madj pos dst
+        UM.unsafeWrite mectx pos w
+    adjacentCSR <- U.unsafeFreeze madj
+    edgeCtxCSR <- U.unsafeFreeze mectx
+    return CSR{..}
+
+adj :: SparseGraph w -> Vertex -> U.Vector Vertex
+adj CSR{..} v = U.unsafeSlice o (o' - o) adjacentCSR
+  where
+    o = U.unsafeIndex offsetCSR v
+    o' = U.unsafeIndex offsetCSR (v + 1)
+{-# INLINE adj #-}
+
+adjW :: (U.Unbox w)
+    => SparseGraph w -> Vertex -> U.Vector (Vertex, w)
+adjW CSR{..} v = U.zip
+    (U.unsafeSlice o (o' - o) adjacentCSR)
+    (U.unsafeSlice o (o' - o) edgeCtxCSR)
+  where
+    o = U.unsafeIndex offsetCSR v
+    o' = U.unsafeIndex offsetCSR (v + 1)
+{-# INLINE adjW #-}
+
+
+
