03279fa3ce624602e4c54d0d20aabd5e099a3b7f
cojna <cojna@users.noreply.github.com>
2019-10-20 22:28:57 +0900

    Safe IntMod (#57)

---
+++ test/Data/IntMod/SafeSpec.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ViewPatterns #-}
+
+module Data.IntMod.SafeSpec (main, spec) where
+
+import           Data.Coerce
+import           Data.Int
+import           Data.IntMod.Safe
+import           Data.Proxy
+import           Test.Prelude
+import           Test.Prop.Num
+import           Test.Prop.Integral
+import           Test.Prop.Fractional
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+    numSpec (Proxy :: Proxy IntMod)
+    numClosedSpec intModValidate
+    integralSpec (Proxy :: Proxy IntMod)
+    fractionalSpec (Proxy :: Proxy IntMod)
+    fractionalClosedSpec intModValidate
+
+instance Arbitrary IntMod where
+    arbitrary = coerce . intMod <$> (arbitrary :: Gen Int64)
