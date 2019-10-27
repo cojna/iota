afbf4fdb3d90b9ff79f7487ea5693c0fc59d71f3
cojna <cojna@users.noreply.github.com>
2019-10-27 19:41:40 +0900

    test with timelimit

--- test/Test/Prelude.hs
+++ test/Test/Prelude.hs
@@ -7,22 +7,35 @@ module Test.Prelude
     , module Test.QuickCheck.Arbitrary
     , module Test.QuickCheck.Monadic
     , evaluate
+    , withTLEmsec
     , Prime(..)
     , ByteStringOf(..)
     ) where
 
-import           Control.Exception         (evaluate)
+import           Control.Exception         (Exception (..), evaluate, throwIO)
 import qualified Data.ByteString.Char8     as C
 import           Data.Coerce
 import           Data.IntMod
 import           Data.Proxy
 import           GHC.TypeLits
 import           Math.Prime                (smallPrimes)
+import           System.Timeout            (timeout)
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
 import           Test.QuickCheck.Arbitrary
 import           Test.QuickCheck.Monadic
+
+data TimeLimitExceeded = TimeLimitExceeded deriving (Show)
+
+instance Exception TimeLimitExceeded
+
+withTLEmsec :: Int -> IO a -> IO a
+withTLEmsec msec action = do
+    res <- timeout (msec * 1000) action
+    case res of
+        Just x  -> return x
+        Nothing -> throwIO TimeLimitExceeded
 
 newtype Prime a = Prime {getPrime :: a}
     deriving (Eq, Ord, Show)
