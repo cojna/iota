424e095ab3ad2b81a2df9813d28df34da5bba4b0
cojna <cojna@users.noreply.github.com>
2020-04-19 19:01:19 +0900

    Eratosthenes sieve

--- src/Math/Prime.hs
+++ src/Math/Prime.hs
@@ -1,9 +1,16 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE Safe         #-}
+{-# LANGUAGE BangPatterns, BinaryLiterals, TypeApplications #-}
+
 
 module Math.Prime where
 
-import qualified Data.List as L
+import           Control.Monad
+import           Control.Monad.ST
+import           Data.Bits
+import           Data.Function
+import qualified Data.List           as L
+import           Data.Primitive
+import qualified Data.Vector.Unboxed as U
+import           Data.Word
 
 smallPrimes :: [Int]
 smallPrimes = 2 : [ n | n<-[3,5..46337], all ((>0).rem n) $ takeWhile (\x->x*x<=n) smallPrimes]
@@ -30,3 +37,34 @@ totient n = n `quot` product ps * product (map (subtract 1) ps)
 
 divisors :: Int -> [Int]
 divisors n = L.sort . map product . mapM (scanl (*) 1) . L.group $ primeFactors n
+
+withPrimes :: Int -> (U.Vector Int -> a) -> a
+withPrimes n f = f . U.filter isP $ U.generate (n + 1) id
+  where
+    !(Sieve sieved) = sieve n
+    isP i =
+        let seg = indexByteArray @Word64 sieved (unsafeShiftR i 6)
+        in testBit seg (i .&. 0x3f)
+
+newtype Sieve = Sieve ByteArray
+
+sieve :: Int -> Sieve
+sieve n = runST $ do
+    let lim = ((n + 1) + 63) `quot` 64 * 64
+    isp <- newByteArray (lim * 8)
+    fillByteArray isp 0 (lim * 8) 0b10101010
+    seg0 <- readByteArray @Word64 isp 0
+    writeByteArray @Word8 isp 0 $ 0b10101100
+    let !sqrtLim = floor . sqrt $ fromIntegral lim
+    flip fix 3 $ \loop !p -> do
+        seg <- readByteArray @Word64 isp (unsafeShiftR p 6)
+        when (testBit seg (p .&. 0x3f)) $ do
+            flip fix (p * p) $ \loop' !i -> do
+                when (i < lim) $ do
+                    seg' <- readByteArray @Word64 isp (unsafeShiftR i 6)
+                    writeByteArray @Word64 isp (unsafeShiftR i 6)
+                        $ clearBit seg' (i .&. 0x3f)
+                    loop' (i + 2 * p)
+        when (p + 2 <= sqrtLim) $ do
+            loop (p + 2)
+    Sieve <$> unsafeFreezeByteArray isp
