c0cc8401a95e0877831ecf3961c9945af40eec0b
cojna <cojna@users.noreply.github.com>
2020-05-06 18:07:01 +0900

    Use Interactive

--- src/Control/GCJ.hs
+++ src/Control/GCJ.hs
@@ -1,11 +1,15 @@
-{-# LANGUAGE ConstraintKinds, ImplicitParams, LambdaCase, RankNTypes #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Control.GCJ where
 
+import           Control.Applicative
 import           Control.Monad
+import           Control.Monad.Trans
 import           System.Environment
 import           System.IO
 import           System.Process
+
+import           Control.Monad.Interactive
 
 formatGCJ :: Int -> String
 formatGCJ i = "Case #" <> shows i ": "
@@ -18,46 +22,7 @@ withGCJ f = getArgs >>= \case
         mapM_ ((*> f) . putStrLn . formatGCJ) [1..t]
     args -> error $ show args
 
-data InteractiveJudge = InteractiveJudge
-    { hin :: Handle
-    , hout :: Handle
-    }
-
-type HasInteractiveJudge = (?interactiveJudge :: InteractiveJudge)
-
-withInteractiveJudge :: (HasInteractiveJudge => IO ()) -> IO ()
-withInteractiveJudge f = do
-    judge <- newInteractiveJudge
-    let ?interactiveJudge = judge
-    (t:_) <- map read.words <$> recvLine
-    mapM_ ((*> f) . hPutStrLn stderr . formatGCJ) [1..t]
-
-newInteractiveJudge :: IO InteractiveJudge
-newInteractiveJudge = getArgs >>= \case
-    [] -> return $ InteractiveJudge stdin stdout
-    args -> do
-        (Just i, Just o, _, _) <- createProcess
-            (shell . unwords $ "python3 local_testing_tool.py": args)
-                { std_in = CreatePipe
-                , std_out = CreatePipe
-                }
-        return $ InteractiveJudge o i
-
-send :: (Show a, HasInteractiveJudge) => a -> IO ()
-send = sendStrLn . show
-
-sendStr :: HasInteractiveJudge => String -> IO ()
-sendStr cs = do
-    hPutStr (hout ?interactiveJudge) cs
-    hFlush (hout ?interactiveJudge)
-
-sendStrLn :: HasInteractiveJudge => String -> IO ()
-sendStrLn cs = do
-    hPutStrLn (hout ?interactiveJudge) cs
-    hFlush (hout ?interactiveJudge)
-
-recvLine :: HasInteractiveJudge => IO String
-recvLine = hGetLine (hin ?interactiveJudge)
-
-recvLn :: (Read a, HasInteractiveJudge) => IO a
-recvLn = read <$> recvLine
+withGCJInteractive :: ([Int] -> Interactive a) -> IO ()
+withGCJInteractive f = withInteractive $ do
+    (t:params) <- map read.words <$> lift recvLine
+    foldr ((<|>).const (f params)) empty [1..t]
