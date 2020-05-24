23774ee76db18e24cd247d7ccfa68f5e63d0bca8
cojna <cojna@users.noreply.github.com>
2020-05-24 23:51:12 +0900

    Add Map based memo

--- test/Control/Memo/FixSpec.hs
+++ test/Control/Memo/FixSpec.hs
@@ -14,3 +14,14 @@ spec = do
                 fib memo 1 = 1
                 fib memo i = memo (i - 1) + memo (i - 2)
             memoFix 100 fib 50 `shouldBe` 12586269025
+    describe "memoFixMap" $ do
+        it "calculate fib 50 with memoized recursion" $ do
+            memoFixMap fibM 50 `shouldBe` 12586269025
+    describe "memoFixIntMap" $ do
+        it "calculate fib 50 with memoized recursion" $ do
+            memoFixIntMap fibM 50 `shouldBe` 12586269025
+
+fibM :: (Monad m) => (Int -> m Integer) -> Int -> m Integer
+fibM _ 0 = pure 0
+fibM _ 1 = pure 1
+fibM memo i = (+) <$> memo (i - 1) <*> memo (i - 2)
