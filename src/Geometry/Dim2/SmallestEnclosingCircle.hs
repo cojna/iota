fe35f9281fbf47d9a0e1709150ff4e9a3fac2629
cojna <cojna@users.noreply.github.com>
2020-05-26 01:21:46 +0900

    Fix not on border bug

--- src/Geometry/Dim2/SmallestEnclosingCircle.hs
+++ src/Geometry/Dim2/SmallestEnclosingCircle.hs
@@ -10,15 +10,23 @@ import           Geometry.Dim2.Circle
 import           System.Random.XoRoShiRo
 
 smallestEnclosingCircle :: (G.Vector v Point) => v Point -> Circle
-smallestEnclosingCircle = welzl [] . G.toList -- . shuffle
+smallestEnclosingCircle = welzl [] . G.toList . shuffle
   where
-    welzl rs [] = naiveSmallestEnclosingCircle rs
-    welzl rs@[_,_,_] _ = naiveSmallestEnclosingCircle rs
+    welzl rs@[_,_,_] ps = trivial rs
+    welzl rs [] = trivial rs
     welzl rs (p:ps)
         | inCircle p c = c
         | otherwise = welzl (p:rs) ps
       where
-        c = welzl rs ps
+        !c = welzl rs ps
+
+    trivial [] = Circle (P 0.0 0.0) 0.0
+    trivial [p] = Circle p 0.0
+    trivial [p0, p1] = Circle (0.5 *: (p0 + p1)) (norm2 (p1 - p0) * 0.5)
+    -- not equal to the smallest enclosing circle
+    trivial [p0, p1, p2] = Circle c (norm2 (p0 - c))
+      where
+        c = circumcenter p0 p1 p2
 
 naiveSmallestEnclosingCircle :: [Point] -> Circle
 naiveSmallestEnclosingCircle [] = Circle (P 0.0 0.0) 0.0
--- test/Geometry/Dim2/SmallestEnclosingCircleSpec.hs
+++ test/Geometry/Dim2/SmallestEnclosingCircleSpec.hs
@@ -20,6 +20,15 @@ spec = do
         prop "enclose all points" prop_enclose
         prop "equal to naive" prop_naive
         prop "equal to golden section search" prop_goldenSectionSearch
+        describe "corner cases" $ do
+            it "only two points on boundary" $ do
+              let points = map (uncurry P) [(-1,0),(1,0),(0,0.5)]
+              smallestEnclosingCircle (V.fromList points)
+                `shouldBe` Circle (P 0 0) 1
+            it "on boundary invaliant" $ do
+              let points = map (uncurry P) [(7,6),(5,-7),(8,-2),(-3,-6),(-7,6)]
+              smallestEnclosingCircle (V.fromList points)
+                `shouldBe` Circle (P 0.0 0.4230769230769231) 8.949976033818032
 
 byGoldenSectionSearch :: V.Vector Point -> Circle
 byGoldenSectionSearch points | V.null points = Circle (P 0.0 0.0) 0.0
@@ -28,13 +37,13 @@ byGoldenSectionSearch points = Circle (P x y) r
     low = -1000.0
     high = 1000.0
     Min (Arg r (x, y)) = goldenSectionSearchMin2 low high $ \cx cy ->
-            let !c = P cx cy
-            in V.maximum $ V.map (\p -> norm2 (p - c)) points
+        let !c = P cx cy
+        in V.maximum $ V.map (\p -> norm2 (p - c)) points
 
 prop_naive :: SizeBoundedList 16 Point -> Bool
 prop_naive (getSizeBoundedList -> points)
     = smallestEnclosingCircle (V.fromList points) ==
-            naiveSmallestEnclosingCircle points
+        naiveSmallestEnclosingCircle points
 
 prop_goldenSectionSearch :: [Point] -> Bool
 prop_goldenSectionSearch (V.fromList -> points)
