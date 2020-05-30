b459ad3124c875590232510c543e345f5d6f40d0
cojna <cojna@users.noreply.github.com>
2020-05-30 15:07:30 +0900

    Refactor BinarySearch

--- src/Algorithm/BinarySearch.hs
+++ src/Algorithm/BinarySearch.hs
@@ -2,29 +2,42 @@
 
 module Algorithm.BinarySearch where
 
-import           Control.Exception
+import           Data.Functor.Identity
 
-lowerBound :: (Integral i) => i -> i -> (i -> Bool) -> i
-lowerBound low high p = assert (p high) $ go low high
+import           Utils                 (unsafeShiftRL)
+
+-- | assert (p high)
+lowerBound :: Int -> Int -> (Int -> Bool) -> Int
+lowerBound low high p = go low high
   where
     go !low !high
         | high <= low = high
         | p mid       = go low       mid
         | otherwise   = go (mid + 1) high
       where
-        h = toInteger high
-        l = toInteger low
-        mid = fromIntegral $ l + div (h - l) 2
+        mid = low + unsafeShiftRL (high - low) 1
 {-# INLINE lowerBound #-}
 
-upperBound :: (Integral i) => i -> i -> (i -> Bool) -> i
+-- | assert (p low)
+upperBound :: Int -> Int -> (Int -> Bool) -> Int
 upperBound low high p
     | p high = high
     | otherwise = lowerBound low high (not.p) - 1
 {-# INLINE upperBound #-}
 
-lowerBoundM :: (Integral i, Monad m) => i -> i -> (i -> m Bool) -> m i
-lowerBoundM low high p = go low high
+lowerBoundInteger :: Integer -> Integer -> (Integer -> Bool) -> Integer
+lowerBoundInteger low high p = runIdentity
+    $ lowerBoundIntegerM low high (pure . p)
+{-# INLINE lowerBoundInteger #-}
+
+upperBoundInteger :: Integer -> Integer -> (Integer -> Bool) -> Integer
+upperBoundInteger low high p = runIdentity
+    $ upperBoundIntegerM low high (pure . p)
+{-# INLINE upperBoundInteger #-}
+
+lowerBoundIntegerM
+    :: (Monad m) => Integer -> Integer -> (Integer -> m Bool) -> m Integer
+lowerBoundIntegerM low high p = go low high
   where
     go !low !high
         | high <= low = return high
@@ -38,18 +51,20 @@ lowerBoundM low high p = go low high
         l = toInteger low
         mid = fromIntegral $ l + div (h - l) 2
 
-{-# INLINE lowerBoundM #-}
+{-# INLINE lowerBoundIntegerM #-}
 
-upperBoundM :: (Integral i, Monad m) => i -> i -> (i -> m Bool) -> m i
-upperBoundM low high p = do
+upperBoundIntegerM
+    :: (Monad m) => Integer -> Integer -> (Integer -> m Bool) -> m Integer
+upperBoundIntegerM low high p = do
     phigh <- p high
     if phigh
     then return high
-    else subtract 1 <$> lowerBoundM low high (fmap not.p)
-{-# INLINE upperBoundM #-}
+    else subtract 1 <$> lowerBoundIntegerM low high (fmap not.p)
+{-# INLINE upperBoundIntegerM #-}
 
+-- | assert (p high)
 lowerBoundDouble :: Double -> Double -> (Double -> Bool) -> Double
-lowerBoundDouble low high p = assert (p high) $ go 50 low high
+lowerBoundDouble low high p = go 50 low high
   where
     go !n !low !high
         | n == 0    = high
@@ -58,8 +73,9 @@ lowerBoundDouble low high p = assert (p high) $ go 50 low high
       where
         mid = (low + high) * 0.5
 
+-- | assert (p low)
 upperBoundDouble :: Double -> Double -> (Double -> Bool) -> Double
-upperBoundDouble low high p = assert (p low) $ go 50 low high
+upperBoundDouble low high p = go 50 low high
   where
     go !n !low !high
         | n == 0    = low
