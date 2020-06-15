af41ffea4d22ea6d73223b6e8d69259f686f186b
cojna <cojna@users.noreply.github.com>
2020-06-15 13:14:39 +0900

    Fix newSegTree (#80)
    * Fix the SegTree size
    
    * Add mappendAll
    
    * Add readSegTree
    
    * Add header
    
    * Fix initialization
    
    * Refactor SegTree
    
    * Update SegTree spec

--- src/Data/SegTree.hs
+++ src/Data/SegTree.hs
@@ -1,5 +1,10 @@
 {-# LANGUAGE BangPatterns, LambdaCase #-}
 
+-- |
+-- = Segment Tree
+-- == Reference
+--   * <https://codeforces.com/blog/entry/18051>
+
 module Data.SegTree where
 
 import           Control.Monad
@@ -14,22 +19,14 @@ import qualified Data.Vector.Unboxed         as U
 import qualified Data.Vector.Unboxed.Mutable as UM
 import           Data.Word
 --
-import           Utils                       (rev, unsafeShiftRL)
-
--- |
--- >>> extendToPowerOfTwo 0
--- 1
-extendToPowerOfTwo :: Int -> Int
-extendToPowerOfTwo x
-    | x > 1 = unsafeShiftRL (-1) (countLeadingZeros (x - 1)) + 1
-    | otherwise = 1
+import           Utils                       (unsafeShiftRL)
 
 newtype SegTree s a = SegTree { getSegTree :: UM.MVector s a }
 
 newSegTree
     :: (Monoid a, U.Unbox a, PrimMonad m)
     => Int -> m (SegTree (PrimState m) a)
-newSegTree n = SegTree <$> UM.replicate (extendToPowerOfTwo n) mempty
+newSegTree n = SegTree <$> UM.replicate (2 * extendToPowerOfTwo n) mempty
 
 -- | /O(n)/
 buildSegTree
@@ -39,12 +36,20 @@ buildSegTree vec = do
     let n = extendToPowerOfTwo $ U.length vec
     tree <- UM.replicate (2 * n) mempty
     U.unsafeCopy (UM.unsafeSlice n (U.length vec) tree) vec
-    rev (n - 1) $ \i -> do
+    U.forM_ (U.iterateN (n - 1) (subtract 1) (n - 1)) $ \i -> do
         x <- mappend
             <$> UM.unsafeRead tree (unsafeShiftL i 1)
             <*> UM.unsafeRead tree (unsafeShiftL i 1 .|. 1)
         UM.unsafeWrite tree i x
     return $ SegTree tree
+
+-- | /O(1)/
+readSegTree :: (Monoid a, U.Unbox a, PrimMonad m)
+    => SegTree (PrimState m) a -> Int -> m a
+readSegTree segtree k = do
+    let tree = getSegTree segtree
+    let n = unsafeShiftRL (UM.length tree) 1
+    UM.unsafeRead tree (k + n)
 
 -- | /O(log n)/
 writeSegTree
@@ -54,6 +59,22 @@ writeSegTree segtree k v = do
     let tree = getSegTree segtree
     let n = unsafeShiftRL (UM.length tree) 1
     UM.unsafeWrite tree (k + n) v
+    flip fix (k + n) $ \loop !i ->
+        when (i > 1) $ do
+            x <- mappend
+                <$> UM.unsafeRead tree i
+                <*> UM.unsafeRead tree (i `xor` 1)
+            UM.unsafeWrite tree (unsafeShiftRL i 1) x
+            loop $ unsafeShiftR i 1
+
+-- | /O(log n)/
+modifySegTree
+    :: (Monoid a, U.Unbox a, PrimMonad m)
+    => SegTree (PrimState m) a -> (a -> a) -> Int -> m ()
+modifySegTree segtree f k = do
+    let tree = getSegTree segtree
+    let n = unsafeShiftRL (UM.length tree) 1
+    UM.unsafeModify tree f (k + n)
     flip fix (k + n) $ \loop !i ->
         when (i > 1) $ do
             x <- mappend
@@ -82,7 +103,9 @@ mappendFromTo segtree l r = do
             | otherwise = return
 
         go l r k
-            | l < r = go (unsafeShiftRL (l + l .&. 1) 1) (unsafeShiftRL (r - r .&. 1) 1)
+            | l < r
+                = go (unsafeShiftRL (l + l .&. 1) 1)
+                     (unsafeShiftRL (r - r .&. 1) 1)
                 $ stepL l >=> (stepR r >=> k)
             | otherwise = k
     go (n + l) (n + r) return mempty
@@ -95,3 +118,20 @@ mappendTo
     => SegTree (PrimState m) a -> Int -> m a
 mappendTo segtree = mappendFromTo segtree 0
 {-# INLINE mappendTo #-}
+
+-- | mappend [0..n)
+--
+-- /O(1)/
+mappendAll
+    :: (U.Unbox a, PrimMonad m)
+    => SegTree (PrimState m) a -> m a
+mappendAll segtree = UM.unsafeRead (getSegTree segtree) 1
+{-# INLINE mappendAll #-}
+
+-- |
+-- >>> extendToPowerOfTwo 0
+-- 1
+extendToPowerOfTwo :: Int -> Int
+extendToPowerOfTwo x
+    | x > 1 = unsafeShiftRL (-1) (countLeadingZeros (x - 1)) + 1
+    | otherwise = 1
--- test/Data/SegTreeSpec.hs
+++ test/Data/SegTreeSpec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE BangPatterns, CPP, DerivingStrategies, DerivingVia, LambdaCase #-}
-{-# LANGUAGE OverloadedLists, StandaloneDeriving, ViewPatterns              #-}
+{-# LANGUAGE OverloadedLists, StandaloneDeriving, TypeApplications          #-}
+{-# LANGUAGE ViewPatterns                                                   #-}
 
 module Data.SegTreeSpec (main, spec) where
 
@@ -19,24 +20,54 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+    describe "buildSegTree" $ do
+        prop "naive" prop_buildSegTree
+    describe "readSegTree/writeSegTree" $ do
+        it "{write 0 v; read 0} == v" $ do
+            let v = 123
+            seg <- buildSegTree @(Min Int) [10..15]
+            writeSegTree seg 0 v
+            res <- readSegTree seg 0
+            res `shouldBe` v
+        it "{write (n-1) v; read (n-1)} == v" $ do
+            let n = 4
+            let v = 123
+            seg <- buildSegTree @(Min Int) [10..13]
+            writeSegTree seg (n - 1) v
+            res <- readSegTree seg (n - 1)
+            res `shouldBe` v
+    describe "readSegTree/modifySegTree" $ do
+        it "{modify (const v) 0; read 0} == v" $ do
+            let v = 123
+            seg <- buildSegTree @(Min Int) [10..15]
+            modifySegTree seg (const v) 0
+            res <- readSegTree seg 0
+            res `shouldBe` v
+        it "{modify (const v) (n-1); read (n-1)} == v" $ do
+            let n = 4
+            let v = 123
+            seg <- buildSegTree @(Min Int) [10..13]
+            modifySegTree seg (const v) (n - 1)
+            res <- readSegTree seg (n - 1)
+            res `shouldBe` v
     describe "runSegTree" $ do
         prop "naive" $ prop_runNaiveSegTree
         it "minimum [0, 5) for [0..4]" $
-            runSegTree [0..4] [SegQuery 0 5] `shouldBe` U.fromList [0 :: Int]
+            runSegTree [0..4] [SegQuery 0 5] `shouldBe` U.fromList [0 :: Min Int]
         it "minimum [3, 4) for [0..4]" $
-            runSegTree [0..4] [SegQuery 3 4] `shouldBe` U.fromList [3 :: Int]
+            runSegTree [0..4] [SegQuery 3 4] `shouldBe` U.fromList [3 :: Min Int]
         it "minimum [3, 4) for update 3 (-1) [0..4]" $
             runSegTree [0..4] [SegUpdate 3 (-1), SegQuery 3 4]
-                `shouldBe` U.fromList [(-1) :: Int]
+                `shouldBe` U.fromList [(-1) :: Min Int]
         it "minimum [0, 5) for update 3 (-1) [0..4]" $
             runSegTree [0..4] [SegUpdate 3 (-1), SegQuery 0 5]
-                `shouldBe` U.fromList [(-1) :: Int]
+                `shouldBe` U.fromList [(-1) :: Min Int]
         it "minimum [0, 1) for [0]" $
             runSegTree [0] [SegQuery 0 1]
-                `shouldBe` U.fromList [0 :: Int]
+                `shouldBe` U.fromList [0 :: Min Int]
         it "minimum [0, 1) for update 0 (-1) [0]" $
             runSegTree [0] [SegUpdate 0 (-1), SegQuery 0 1]
-                `shouldBe` U.fromList [(-1) :: Int]
+                `shouldBe` U.fromList [(-1) :: Min Int]
     describe "extendToPowerOfTwo" $ do
         it "f 0 == 1" $ extendToPowerOfTwo 0 `shouldBe` 1
         it "f 1 == 1" $ extendToPowerOfTwo 1 `shouldBe` 1
@@ -45,9 +76,6 @@ spec = do
         prop "popCount (f x) == 1" $ prop_powerOfTwo
         prop "x <= f x" $ prop_greaterThanOrEqual
         prop "popCount (f x / 2) /= 1 || f x / 2 < x" $ prop_least
-
-deriving via (Min Int) instance Semigroup Int
-deriving via (Min Int) instance Monoid Int
 
 data SegTreeQuery a
     = SegUpdate !Int !a
@@ -68,7 +96,6 @@ runSegTree vec queries = U.create $ do
                 return $ acc + 1
         ) 0 queries
     return $ UM.take size res
-
 
 runNaiveSegTree
     :: (Monoid a, U.Unbox a)
@@ -93,15 +120,26 @@ prop_runNaiveSegTree :: NonEmptyList Int -> [(Bool, Int, Int)] -> Bool
 prop_runNaiveSegTree (getNonEmpty -> xs) qs
     = runSegTree vec queries == runNaiveSegTree vec queries
   where
-    vec = U.fromList xs
+    vec = U.fromList (map Min xs)
     len = U.length vec
     ix i = mod i len
     queries = V.fromList . flip map qs $ \case
-        (True, k, v) -> SegUpdate (ix k) v
+        (True, k, v) -> SegUpdate (ix k) (Min v)
         (False, l, r) -> SegQuery li (min (li + 1) ri)
           where
             li = ix l `min` ix l
             ri = ix l `min` ix r + 1
+
+prop_buildSegTree :: NonEmptyList Int -> Property
+prop_buildSegTree (U.fromList . map Min . getNonEmpty -> xs) = monadicIO $ do
+    (seg0', seg1') <- run $ do
+        seg0 <- buildSegTree xs
+        seg0' <- U.freeze (getSegTree seg0)
+        seg1 <- newSegTree (U.length xs)
+        U.imapM_ (writeSegTree seg1) xs
+        seg1' <- U.freeze (getSegTree seg1)
+        return (seg0', seg1')
+    assert (seg0' == seg1')
 
 prop_powerOfTwo :: NonNegative Int -> Bool
 prop_powerOfTwo (getNonNegative -> x)
