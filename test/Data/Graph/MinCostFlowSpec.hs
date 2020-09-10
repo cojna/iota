d3af816b868843a09e31fa9f783a4453cda26f4d
cojna <cojna@users.noreply.github.com>
2020-09-10 19:18:57 +0900

    Refactor MinCostFlow

--- test/Data/Graph/MinCostFlowSpec.hs
+++ test/Data/Graph/MinCostFlowSpec.hs
@@ -13,31 +13,31 @@ spec = do
         it "example (can flow)" $ do
             minCostFlow 2 0 1 2 (\builder -> do
                 addEdgeMCFB builder 0 1 123 2
-                ) `shouldBe` Just (246 :: Int)
+                ) `shouldBe` (246 :: Int, 2)
         it "example (cannot flow)" $ do
             minCostFlow 2 0 1 123456789 (\builder -> do
                 addEdgeMCFB builder 0 1 123 2
-                ) `shouldBe` Nothing
+                ) `shouldBe` (246, 2)
         it "multiple edges" $ do
             minCostFlow 2 0 1 1 (\builder -> do
                 addEdgeMCFB builder 0 1 123 1
                 addEdgeMCFB builder 0 1 456 1
-                ) `shouldBe` Just 123
+                ) `shouldBe` (123, 1)
         it "unreachable sink" $ do
             minCostFlow 2 0 1 1 (const $ return ())
-                `shouldBe` Nothing
+                `shouldBe` (0, 0)
         it "contains loop (can flow)" $ do
             minCostFlow 4 0 2 10 (\builder -> do
                 addEdgeMCFB builder 0 1 1 100
                 addEdgeMCFB builder 1 2 1 100
                 addEdgeMCFB builder 2 3 1 100
                 addEdgeMCFB builder 3 0 1 100
-                ) `shouldBe` Just 20
+                ) `shouldBe` (20, 10)
         it "contains loop (cannot flow)" $ do
             minCostFlow 4 0 2 123 (\builder -> do
                 addEdgeMCFB builder 0 1 1 100
                 addEdgeMCFB builder 1 2 1 100
                 addEdgeMCFB builder 2 3 1 100
                 addEdgeMCFB builder 3 0 1 100
-                ) `shouldBe` Nothing
+                ) `shouldBe` (200, 100)
 
