893f813ae206386ad2cd85629d0349c7328d2aad
cojna <cojna@users.noreply.github.com>
2020-09-18 03:17:53 +0900

    Add naive IntMod props

--- test/Data/IntModSpec.hs
+++ test/Data/IntModSpec.hs
@@ -15,8 +15,26 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+    describe "(+%)" $ do
+        prop "naive" prop_naiveAdd
+    describe "(-%)" $ do
+        prop "naive" prop_naiveSub
+    describe "(*%)" $ do
+        prop "naive" prop_naiveTimes
     numSpec (Proxy :: Proxy IntMod)
     numClosedSpec intModValidate
     integralSpec (Proxy :: Proxy IntMod)
     fractionalSpec (Proxy :: Proxy IntMod)
     fractionalClosedSpec intModValidate
+
+prop_naiveAdd :: IntMod -> IntMod -> Bool
+prop_naiveAdd (IntMod x) (IntMod y)
+    = x +% y == mod (x + y) modulus
+
+prop_naiveSub :: IntMod -> IntMod -> Bool
+prop_naiveSub (IntMod x) (IntMod y)
+    = x -% y == mod (x - y) modulus
+
+prop_naiveTimes :: IntMod -> IntMod -> Bool
+prop_naiveTimes (IntMod x) (IntMod y)
+    = x *% y == mod (x * y) modulus
