0f662b53c536db0b85e8e78e14960de626c09c91
cojna <cojna@users.noreply.github.com>
2020-09-20 02:25:35 +0900

    Refactor SegTree

--- src/Data/SegTree/Lazy.hs
+++ src/Data/SegTree/Lazy.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE BangPatterns, MultiParamTypeClasses, TypeApplications #-}
+{-# LANGUAGE BangPatterns, FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE TypeApplications                                       #-}
 
 module Data.SegTree.Lazy where
 
@@ -12,93 +13,108 @@ import qualified Data.Vector.Unboxed.Mutable       as UM
 
 import           Utils                             (stream, streamR,
                                                     unsafeShiftRL)
+-- | * @appMonoid mempty x = x@
+--   * @appMonoid (f <> g) x = appMonoid f (appMonoid g x)@
+class (Monoid f) => MonoidAction f a where
+    appMonoid :: f -> a -> a
 
-data SegTreeLazy s a f = SegTreeLazy
+-- | Range update Range query
+instance (Monoid m) => MonoidAction m m where
+    appMonoid = (<>)
+    {-# INLINE appMonoid #-}
+
+-- | Point update Range query
+instance MonoidAction () m where
+    appMonoid = flip const
+    {-# INLINE appMonoid #-}
+
+-- | * @appMonoid mempty x = x@
+--   * @appMonoid (f <> g) x = appMonoid f (appMonoid g x)@
+--   * @appMonoid f mempty = mempty@
+--   * @appMonoid f (x <> y) = appMonoid f x <> appMonoid f y@
+data SegTree s a f = SegTree
     (UM.MVector s a)
     (UM.MVector s f)
 
-newSegTreeLazy :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, PrimMonad m)
-    => Int -> m (SegTreeLazy (PrimState m) a f)
-newSegTreeLazy n = SegTreeLazy
+newSegTree :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, PrimMonad m)
+    => Int -> m (SegTree (PrimState m) a f)
+newSegTree n = SegTree
     <$> UM.replicate (2 * extendToPowerOfTwo n) mempty
     <*> UM.replicate (extendToPowerOfTwo n) mempty
 
-buildSegTreeLazy :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, PrimMonad m)
-    => U.Vector a -> m (SegTreeLazy (PrimState m) a f)
-buildSegTreeLazy xs = do
+buildSegTree :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, PrimMonad m)
+    => U.Vector a -> m (SegTree (PrimState m) a f)
+buildSegTree xs = do
     tree <- UM.replicate (2 * n) mempty
     lazy <- UM.replicate n mempty
     U.unsafeCopy (UM.unsafeSlice n (U.length xs) tree) xs
-    let seg = SegTreeLazy tree lazy
+    let seg = SegTree tree lazy
     flip MS.mapM_ (streamR 1 n) $ \i -> do
-        updateSegTreeLazy seg i
+        updateSegTree seg i
     return seg
   where
     !n = extendToPowerOfTwo $ U.length xs
 
-class (Monoid f) => MonoidAction f a where
-    appMonoid :: f -> a -> a
-
-appAllAt :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, MonoidAction f a, PrimMonad m)
-    => SegTreeLazy (PrimState m) a f -> Int -> f -> m ()
-appAllAt (SegTreeLazy tree lazy) k f = do
+fullAppAt :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, MonoidAction f a, PrimMonad m)
+    => SegTree (PrimState m) a f -> Int -> f -> m ()
+fullAppAt (SegTree tree lazy) k f = do
     tk <- UM.unsafeModify tree (appMonoid f) k
     when (k < UM.length lazy) $ do
         UM.unsafeModify lazy (mappend f) k
-{-# INLINE appAllAt #-}
+{-# INLINE fullAppAt #-}
 
-pushSegTreeLazy :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, MonoidAction f a, PrimMonad m)
-    => SegTreeLazy (PrimState m) a f -> Int  -> m ()
-pushSegTreeLazy st@(SegTreeLazy tree lazy) k = do
+pushSegTree :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, MonoidAction f a, PrimMonad m)
+    => SegTree (PrimState m) a f -> Int  -> m ()
+pushSegTree st@(SegTree tree lazy) k = do
     fk <- UM.unsafeRead lazy k
-    appAllAt st (2 * k) fk
-    appAllAt st (2 * k + 1) fk
+    fullAppAt st (2 * k) fk
+    fullAppAt st (2 * k + 1) fk
     UM.unsafeWrite lazy k mempty
-{-# INLINE pushSegTreeLazy #-}
+{-# INLINE pushSegTree #-}
 
-updateSegTreeLazy :: (Monoid a, U.Unbox a, PrimMonad m)
-    => SegTreeLazy (PrimState m) a f -> Int -> m ()
-updateSegTreeLazy (SegTreeLazy tree _) k = do
+updateSegTree :: (Monoid a, U.Unbox a, PrimMonad m)
+    => SegTree (PrimState m) a f -> Int -> m ()
+updateSegTree (SegTree tree _) k = do
     (<>) <$> UM.unsafeRead tree (2 * k) <*> UM.unsafeRead tree (2 * k + 1)
         >>= UM.unsafeWrite tree k
-{-# INLINE updateSegTreeLazy #-}
+{-# INLINE updateSegTree #-}
 
-writeSegTreeLazy :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, MonoidAction f a, PrimMonad m)
-    => SegTreeLazy (PrimState m) a f -> Int -> a -> m ()
-writeSegTreeLazy st@(SegTreeLazy tree lazy) k0 v = do
+writeSegTree :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, MonoidAction f a, PrimMonad m)
+    => SegTree (PrimState m) a f -> Int -> a -> m ()
+writeSegTree st@(SegTree tree lazy) k0 v = do
     let !n = UM.length lazy
         k = k0 + n
         !h = 64 - countLeadingZeros n
     flip MS.mapM_ (streamR 1 h) $ \i -> do
-        pushSegTreeLazy st (unsafeShiftR k i)
+        pushSegTree st (unsafeShiftR k i)
     UM.unsafeWrite tree k v
     flip MS.mapM_ (stream 1 h) $ \i -> do
-        updateSegTreeLazy st (unsafeShiftR k i)
-{-# INLINE writeSegTreeLazy #-}
+        updateSegTree st (unsafeShiftR k i)
+{-# INLINE writeSegTree #-}
 
-readSegTreeLazy :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, MonoidAction f a, PrimMonad m)
-    => SegTreeLazy (PrimState m) a f -> Int -> m a
-readSegTreeLazy st@(SegTreeLazy tree lazy) k0 = do
+readSegTree :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, MonoidAction f a, PrimMonad m)
+    => SegTree (PrimState m) a f -> Int -> m a
+readSegTree st@(SegTree tree lazy) k0 = do
     let !n = UM.length lazy
         k = k0 + n
         !h = 64 - countLeadingZeros n
     flip MS.mapM_ (streamR 1 h) $ \i -> do
-        pushSegTreeLazy st (unsafeShiftR k i)
+        pushSegTree st (unsafeShiftR k i)
     UM.unsafeRead tree k
-{-# INLINE readSegTreeLazy #-}
+{-# INLINE readSegTree #-}
 
 mappendFromTo :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, MonoidAction f a, PrimMonad m)
-    => SegTreeLazy (PrimState m) a f -> Int -> Int -> m a
-mappendFromTo st@(SegTreeLazy tree lazy) l0 r0 = do
+    => SegTree (PrimState m) a f -> Int -> Int -> m a
+mappendFromTo st@(SegTree tree lazy) l0 r0 = do
     let !n = UM.length lazy
         !l = l0 + n
         !r = r0 + n
         !h = 64 - countLeadingZeros n
     flip MS.mapM_ (streamR 1 h) $ \i -> do
         when (unsafeShiftR l i `unsafeShiftL` i /= l) $ do
-            pushSegTreeLazy st (unsafeShiftR l i)
+            pushSegTree st (unsafeShiftR l i)
         when (unsafeShiftR r i `unsafeShiftL` i /= r) $ do
-            pushSegTreeLazy st (unsafeShiftR r i)
+            pushSegTree st (unsafeShiftR r i)
     let calcL l acc
             | l .&. 1 == 1 =
                 mappend acc <$> UM.unsafeRead tree l
@@ -122,49 +138,49 @@ mappendFromTo st@(SegTreeLazy tree lazy) l0 r0 = do
 {-# INLINE mappendFromTo #-}
 
 mappendAll :: (Monoid a, U.Unbox a, PrimMonad m)
-    => SegTreeLazy (PrimState m) a f -> m a
-mappendAll (SegTreeLazy tree _) = UM.unsafeRead tree 1
+    => SegTree (PrimState m) a f -> m a
+mappendAll (SegTree tree _) = UM.unsafeRead tree 1
 {-# INLINE mappendAll #-}
 
 appAt :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, MonoidAction f a, PrimMonad m)
-    => SegTreeLazy (PrimState m) a f -> Int -> f -> m ()
-appAt st@(SegTreeLazy tree lazy) k0 f = do
+    => SegTree (PrimState m) a f -> Int -> f -> m ()
+appAt st@(SegTree tree lazy) k0 f = do
     let !n = UM.length lazy
         k = k0 + n
         !h = 64 - countLeadingZeros n
     flip MS.mapM_ (streamR 1 h) $ \i -> do
-        pushSegTreeLazy st (unsafeShiftR k i)
+        pushSegTree st (unsafeShiftR k i)
     UM.unsafeModify tree (appMonoid f) k
     flip MS.mapM_ (stream 1 h) $ \i -> do
-        updateSegTreeLazy st (unsafeShiftR k i)
+        updateSegTree st (unsafeShiftR k i)
 {-# INLINE appAt #-}
 
 appFromTo :: (Monoid a, U.Unbox a, Monoid f, U.Unbox f, MonoidAction f a, PrimMonad m)
-    => SegTreeLazy (PrimState m) a f -> Int -> Int -> f -> m ()
-appFromTo st@(SegTreeLazy tree lazy) l0 r0 f = when (l0 < r0) $ do
+    => SegTree (PrimState m) a f -> Int -> Int -> f -> m ()
+appFromTo st@(SegTree tree lazy) l0 r0 f = when (l0 < r0) $ do
     let !n = UM.length lazy
         !l = l0 + n
         !r = r0 + n
         !h = 64 - countLeadingZeros n
     flip MS.mapM_ (streamR 1 h) $ \i -> do
         when (unsafeShiftR l i `unsafeShiftL` i /= l) $ do
-            pushSegTreeLazy st (unsafeShiftRL l i)
+            pushSegTree st (unsafeShiftRL l i)
         when (unsafeShiftR r i `unsafeShiftL` i /= r) $ do
-            pushSegTreeLazy st (unsafeShiftRL (r - 1) i)
+            pushSegTree st (unsafeShiftRL (r - 1) i)
 
     fix (\loop !l' !r' -> when (l' < r') $ do
         when (l' .&. 1 == 1) $ do
-            appAllAt st l' f
+            fullAppAt st l' f
         when (r' .&. 1 == 1) $ do
-            appAllAt st (r' - 1) f
+            fullAppAt st (r' - 1) f
         loop (unsafeShiftRL (l' + l' .&. 1) 1) (unsafeShiftRL (r' - r' .&. 1) 1)
         ) l r
 
     flip MS.mapM_ (stream 1 h) $ \i -> do
         when (unsafeShiftR l i `unsafeShiftL` i /= l) $ do
-            updateSegTreeLazy st (unsafeShiftRL l i)
+            updateSegTree st (unsafeShiftRL l i)
         when (unsafeShiftR r i `unsafeShiftL` i /= r) $ do
-            updateSegTreeLazy st (unsafeShiftRL (r - 1) i)
+            updateSegTree st (unsafeShiftRL (r - 1) i)
 {-# INLINE appFromTo #-}
 
 extendToPowerOfTwo :: Int -> Int
