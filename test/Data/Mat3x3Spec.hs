84be329827d66881762e88ed9594abc53163dae6
cojna <cojna@users.noreply.github.com>
2021-02-06 19:11:03 +0900

    Mat3x3

---
+++ test/Data/Mat3x3Spec.hs
@@ -0,0 +1,19 @@
+module Data.Mat3x3Spec (main, spec) where
+
+import Control.Monad
+import Data.IntMod
+import Data.Mat3x3
+import Data.Primitive
+import GHC.Exts (fromList)
+import Test.Prelude
+import Test.Prop.Num
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+    numSpec (Proxy :: Proxy (Mat3x3 IntMod))
+
+instance (Arbitrary a, Prim a) => Arbitrary (Mat3x3 a) where
+    arbitrary = fromList <$> replicateM 9 arbitrary
