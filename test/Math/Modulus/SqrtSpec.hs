fde6ba04f03dc150ce4cca7159a1ab5f934efeb2
cojna <cojna@users.noreply.github.com>
2021-03-09 02:59:15 +0900

    Move sqrtMod

---
+++ test/Math/Modulus/SqrtSpec.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE ViewPatterns #-}
+
+module Math.Modulus.SqrtSpec (main, spec) where
+
+import           Math.Modulus
+import           Math.Modulus.Sqrt
+import           Test.Prelude
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+    describe "sqrtMod" $ do
+        it "sqrtMod 2 1000000007 = [59713600,940286407]" $ do
+            sqrtMod 2 1000000007 `shouldBe` [59713600,940286407]
+        it "sqrtMod 3 1000000007 = [82062379,917937628]" $ do
+            sqrtMod 3 1000000007 `shouldBe` [82062379,917937628]
+        it "sqrtMod 4 1000000007 = [2,1000000005]" $ do
+            sqrtMod 4 1000000007 `shouldBe` [2,1000000005]
+        it "sqrtMod 5 1000000007 = []" $ do
+            sqrtMod 5 1000000007 `shouldBe` []
+        it "sqrtMod 2 998244353 = [116195171,882049182]" $ do
+            sqrtMod 2 998244353 `shouldBe` [116195171,882049182]
+        it "sqrtMod 3 998244353 = []" $ do
+            sqrtMod 3 998244353 `shouldBe` []
+        it "sqrtMod 4 998244353 = [2,998244351]" $ do
+            sqrtMod 4 998244353 `shouldBe`  [2,998244351]
+        it "sqrtMod 5 998244353 = []" $ do
+            sqrtMod 5 998244353 `shouldBe` []
+        prop "(sqrtMod a p) ^ 2 == a" prop_sqrtMod
+
+prop_sqrtMod :: Int -> Prime Int -> Bool
+prop_sqrtMod a (getPrime -> p)
+    = and [res * res `rem` p == mod a p | res <- sqrtMod a p]
--- test/Math/ModulusSpec.hs
+++ test/Math/ModulusSpec.hs
@@ -45,24 +45,6 @@ spec = do
         it "logMod 1 2 1000000007 = Nothing" $ do
             logMod 1 2 1000000007 `shouldBe` Nothing
         prop "a ^ logMod a (a ^ x) == a ^ x" prop_logMod
-    describe "sqrtMod" $ do
-        it "sqrtMod 2 1000000007 = [59713600,940286407]" $ do
-            sqrtMod 2 1000000007 `shouldBe` [59713600,940286407]
-        it "sqrtMod 3 1000000007 = [82062379,917937628]" $ do
-            sqrtMod 3 1000000007 `shouldBe` [82062379,917937628]
-        it "sqrtMod 4 1000000007 = [2,1000000005]" $ do
-            sqrtMod 4 1000000007 `shouldBe` [2,1000000005]
-        it "sqrtMod 5 1000000007 = []" $ do
-            sqrtMod 5 1000000007 `shouldBe` []
-        it "sqrtMod 2 998244353 = [116195171,882049182]" $ do
-            sqrtMod 2 998244353 `shouldBe` [116195171,882049182]
-        it "sqrtMod 3 998244353 = []" $ do
-            sqrtMod 3 998244353 `shouldBe` []
-        it "sqrtMod 4 998244353 = [2,998244351]" $ do
-            sqrtMod 4 998244353 `shouldBe`  [2,998244351]
-        it "sqrtMod 5 998244353 = []" $ do
-            sqrtMod 5 998244353 `shouldBe` []
-        prop "(sqrtMod a p) ^ 2 == a" prop_sqrtMod
     describe "crt" $ do
         it "crt (10, 20) (10, 30) = Just (10, 60)" $ do
             crt (10, 20) (10, 30) `shouldBe` Just (10, 60)
@@ -121,10 +103,6 @@ prop_logMod a0 (getNonNegative -> x) (getPrime -> modulus)
   where
     a = mod a0 modulus
     !ax = powMod a x modulus
-
-prop_sqrtMod :: Int -> Prime Int -> Bool
-prop_sqrtMod a (getPrime -> p)
-    = and [res * res `rem` p == mod a p | res <- sqrtMod a p]
 
 validateCRT' :: (Int, Prime Int) -> (Int, Prime Int) -> Bool
 validateCRT' (r0, p0) (r1, p1)
