0a6b5ffeaf540f52156b4e27508c3177fc2c455e
cojna <cojna@users.noreply.github.com>
2021-05-09 23:43:13 +0900

    Escape newlines

--- test/My/PreludeSpec.hs
+++ test/My/PreludeSpec.hs
@@ -21,18 +21,18 @@ spec = do
       B.toLazyByteString (unwordsB @V.Vector B.intDec [1, 2, 3])
         `shouldBe` "1 2 3"
   describe "unlinesB" $ do
-    it "unlinesB intDec [1,2,3] == \"1\n2\n3\n\"" $
+    it "unlinesB intDec [1,2,3] == \"1\\n2\\n3\\n\"" $
       B.toLazyByteString (unlinesB @V.Vector B.intDec [1, 2, 3])
         `shouldBe` "1\n2\n3\n"
-    it "unlinesB (pairB intDec intDec) [(1,2),(3,4)] == \"1 2\n3 4\n\"" $
+    it "unlinesB (pairB intDec intDec) [(1,2),(3,4)] == \"1 2\\n3 4\\n\"" $
       B.toLazyByteString (unlinesB @V.Vector (pairB B.intDec B.intDec) [(1, 2), (3, 4)])
         `shouldBe` "1 2\n3 4\n"
   describe "matrixB" $ do
-    it "matrixB 2 3 [1..6] == \"1 2 3\n4 5 6\n\"" $
+    it "matrixB 2 3 [1..6] == \"1 2 3\\n4 5 6\\n\"" $
       B.toLazyByteString (matrixB @V.Vector 2 3 B.intDec [1, 2, 3, 4, 5, 6])
         `shouldBe` "1 2 3\n4 5 6\n"
   describe "gridB" $ do
-    it "matrixB 2 3 [1..6] == \"123\n456\n\"" $
+    it "matrixB 2 3 [1..6] == \"123\\n456\\n\"" $
       B.toLazyByteString (gridB @V.Vector 2 3 B.intDec [1, 2, 3, 4, 5, 6])
         `shouldBe` "123\n456\n"
   describe "takeLine @Maybe" $ do
