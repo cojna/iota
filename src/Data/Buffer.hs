6a2bcbbdfde449d653a985bfc98b7038847557f3
cojna <cojna@users.noreply.github.com>
2021-06-12 23:45:11 +0900

    Refactor Buffer

--- src/Data/Buffer.hs
+++ src/Data/Buffer.hs
@@ -1,148 +1,168 @@
-module Data.Deque where
+{-# LANGUAGE NamedFieldPuns #-}
 
+module Data.Buffer where
+
+import Control.Applicative
+import Control.Exception (assert)
 import Control.Monad.Primitive
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as UM
 
-data Deque s a = Deque
-  { dequeVars :: !(UM.MVector s Int)
-  , getDeque :: !(UM.MVector s a)
+data Buffer s a = Buffer
+  { bufferVars :: !(UM.MVector s Int)
+  , internalBuffer :: !(UM.MVector s a)
+  , internalBufferSize :: !Int
   }
 
-_dequeFrontPos :: Int
-_dequeFrontPos = 0
+_bufferFrontPos :: Int
+_bufferFrontPos = 0
 
-_dequeBackPos :: Int
-_dequeBackPos = 1
+_bufferBackPos :: Int
+_bufferBackPos = 1
 
-newDeque :: (PrimMonad m, U.Unbox a) => Int -> m (Deque (PrimState m) a)
-newDeque n = Deque <$> UM.replicate 2 n <*> UM.unsafeNew (2 * n)
+newBuffer :: (U.Unbox a, PrimMonad m) => Int -> m (Buffer (PrimState m) a)
+newBuffer n = Buffer <$> UM.replicate 2 0 <*> UM.unsafeNew n <*> pure n
 
-defaultDequeSize :: Int
-defaultDequeSize = 1024 * 1024
+type Stack s a = Buffer s a
+newBufferAsStack :: (U.Unbox a, PrimMonad m) => Int -> m (Buffer (PrimState m) a)
+newBufferAsStack n = Buffer <$> UM.replicate 2 0 <*> UM.unsafeNew n <*> pure n
 
-type Queue s a = Deque s a
+type Queue s a = Buffer s a
+newBufferAsQueue :: (U.Unbox a, PrimMonad m) => Int -> m (Buffer (PrimState m) a)
+newBufferAsQueue n = Buffer <$> UM.replicate 2 0 <*> UM.unsafeNew n <*> pure n
 
-newQueue :: (PrimMonad m, U.Unbox a) => Int -> m (Deque (PrimState m) a)
-newQueue = newBuffer
+type Deque s a = Buffer s a
+newBufferAsDeque :: (U.Unbox a, PrimMonad m) => Int -> m (Buffer (PrimState m) a)
+newBufferAsDeque n =
+  Buffer <$> UM.replicate 2 n
+    <*> UM.unsafeNew (2 * n)
+    <*> pure (2 * n)
 
-clearQueue :: (U.Unbox a, PrimMonad m) => Deque (PrimState m) a -> m ()
-clearQueue = clearBuffer
+lengthBuffer :: (PrimMonad m) => Buffer (PrimState m) a -> m Int
+lengthBuffer Buffer{bufferVars} =
+  liftA2
+    (-)
+    (UM.unsafeRead bufferVars _bufferBackPos)
+    (UM.unsafeRead bufferVars _bufferFrontPos)
+{-# INLINE lengthBuffer #-}
 
-type Stack s a = Deque s a
-
-newStack :: (PrimMonad m, U.Unbox a) => Int -> m (Deque (PrimState m) a)
-newStack = newBuffer
-
-clearStack :: (U.Unbox a, PrimMonad m) => Deque (PrimState m) a -> m ()
-clearStack = clearBuffer
-
-type Buffer s a = Deque s a
-
-newBuffer :: (PrimMonad m, U.Unbox a) => Int -> m (Deque (PrimState m) a)
-newBuffer n = Deque <$> UM.replicate 2 0 <*> UM.unsafeNew n
-
-lengthBuffer :: (PrimMonad m) => Deque (PrimState m) a -> m Int
-lengthBuffer = lengthDeque
-
-clearBuffer :: (PrimMonad m) => Deque (PrimState m) a -> m ()
-clearBuffer (Deque vars _) = do
-  UM.unsafeWrite vars _dequeFrontPos 0
-  UM.unsafeWrite vars _dequeBackPos 0
+clearBuffer :: (PrimMonad m) => Buffer (PrimState m) a -> m ()
+clearBuffer Buffer{bufferVars} = do
+  UM.unsafeWrite bufferVars _bufferFrontPos 0
+  UM.unsafeWrite bufferVars _bufferBackPos 0
 
 freezeBuffer ::
-  (PrimMonad m, U.Unbox a) =>
-  Deque (PrimState m) a ->
+  (U.Unbox a, PrimMonad m) =>
+  Buffer (PrimState m) a ->
   m (U.Vector a)
-freezeBuffer (Deque info v) = do
-  b <- UM.unsafeRead info _dequeBackPos
-  U.freeze $ UM.unsafeTake b v
+freezeBuffer Buffer{bufferVars, internalBuffer} = do
+  f <- UM.unsafeRead bufferVars _bufferFrontPos
+  b <- UM.unsafeRead bufferVars _bufferBackPos
+  U.freeze $ UM.unsafeSlice f (b - f) internalBuffer
 
 unsafeFreezeBuffer ::
-  (PrimMonad m, U.Unbox a) =>
-  Deque (PrimState m) a ->
+  (U.Unbox a, PrimMonad m) =>
+  Buffer (PrimState m) a ->
   m (U.Vector a)
-unsafeFreezeBuffer (Deque info v) = do
-  b <- UM.unsafeRead info _dequeBackPos
-  U.unsafeFreeze $ UM.unsafeTake b v
+unsafeFreezeBuffer Buffer{bufferVars, internalBuffer} = do
+  f <- UM.unsafeRead bufferVars _bufferFrontPos
+  b <- UM.unsafeRead bufferVars _bufferBackPos
+  U.unsafeFreeze $ UM.unsafeSlice f (b - f) internalBuffer
 
-lengthDeque :: (PrimMonad m) => Deque (PrimState m) a -> m Int
-lengthDeque (Deque info _) =
-  (-) <$> UM.unsafeRead info _dequeBackPos
-    <*> UM.unsafeRead info _dequeFrontPos
-{-# INLINE lengthDeque #-}
+freezeInternalBuffer ::
+  (U.Unbox a, PrimMonad m) =>
+  Buffer (PrimState m) a ->
+  m (U.Vector a)
+freezeInternalBuffer Buffer{bufferVars, internalBuffer} = do
+  b <- UM.unsafeRead bufferVars _bufferBackPos
+  U.freeze $ UM.unsafeSlice 0 b internalBuffer
 
-popFront :: (PrimMonad m, U.Unbox a) => Deque (PrimState m) a -> m (Maybe a)
-popFront (Deque info v) = do
-  f <- UM.unsafeRead info _dequeFrontPos
-  b <- UM.unsafeRead info _dequeBackPos
+unsafeFreezeInternalBuffer ::
+  (U.Unbox a, PrimMonad m) =>
+  Buffer (PrimState m) a ->
+  m (U.Vector a)
+unsafeFreezeInternalBuffer Buffer{bufferVars, internalBuffer} = do
+  b <- UM.unsafeRead bufferVars _bufferBackPos
+  U.unsafeFreeze $ UM.unsafeSlice 0 b internalBuffer
+
+popFront :: (U.Unbox a, PrimMonad m) => Buffer (PrimState m) a -> m (Maybe a)
+popFront Buffer{bufferVars, internalBuffer} = do
+  f <- UM.unsafeRead bufferVars _bufferFrontPos
+  b <- UM.unsafeRead bufferVars _bufferBackPos
   if f < b
     then do
-      UM.unsafeWrite info _dequeFrontPos (f + 1)
-      pure <$> UM.unsafeRead v f
+      UM.unsafeWrite bufferVars _bufferFrontPos (f + 1)
+      pure <$> UM.unsafeRead internalBuffer f
     else return Nothing
 {-# INLINE popFront #-}
 
-popBack :: (PrimMonad m, U.Unbox a) => Deque (PrimState m) a -> m (Maybe a)
-popBack (Deque info v) = do
-  f <- UM.unsafeRead info _dequeFrontPos
-  b <- UM.unsafeRead info _dequeBackPos
+viewFront :: (U.Unbox a, PrimMonad m) => Buffer (PrimState m) a -> m (Maybe a)
+viewFront Buffer{bufferVars, internalBuffer} = do
+  f <- UM.unsafeRead bufferVars _bufferFrontPos
+  b <- UM.unsafeRead bufferVars _bufferBackPos
+  if f < b
+    then pure <$> UM.unsafeRead internalBuffer f
+    else return Nothing
+{-# INLINE viewFront #-}
+
+popBack :: (U.Unbox a, PrimMonad m) => Buffer (PrimState m) a -> m (Maybe a)
+popBack Buffer{bufferVars, internalBuffer} = do
+  f <- UM.unsafeRead bufferVars _bufferFrontPos
+  b <- UM.unsafeRead bufferVars _bufferBackPos
   if f < b
     then do
-      UM.unsafeWrite info _dequeBackPos (b - 1)
-      pure <$> UM.unsafeRead v (b - 1)
+      UM.unsafeWrite bufferVars _bufferBackPos (b - 1)
+      pure <$> UM.unsafeRead internalBuffer (b - 1)
     else return Nothing
 {-# INLINE popBack #-}
 
-pushFront :: (PrimMonad m, U.Unbox a) => a -> Deque (PrimState m) a -> m ()
-pushFront x (Deque info v) = do
-  f <- UM.unsafeRead info _dequeFrontPos
-  UM.unsafeWrite v (f - 1) x
-  UM.unsafeWrite info _dequeFrontPos (f - 1)
+viewBack :: (U.Unbox a, PrimMonad m) => Buffer (PrimState m) a -> m (Maybe a)
+viewBack Buffer{bufferVars, internalBuffer} = do
+  f <- UM.unsafeRead bufferVars _bufferFrontPos
+  b <- UM.unsafeRead bufferVars _bufferBackPos
+  if f < b
+    then pure <$> UM.unsafeRead internalBuffer (b - 1)
+    else return Nothing
+{-# INLINE viewBack #-}
+
+pushFront :: (U.Unbox a, PrimMonad m) => a -> Buffer (PrimState m) a -> m ()
+pushFront x Buffer{bufferVars, internalBuffer} = do
+  f <- UM.unsafeRead bufferVars _bufferFrontPos
+  UM.unsafeWrite bufferVars _bufferFrontPos (f - 1)
+  assert (f > 0) $ do
+    UM.unsafeWrite internalBuffer (f - 1) x
 {-# INLINE pushFront #-}
 
-pushBack :: (PrimMonad m, U.Unbox a) => a -> Deque (PrimState m) a -> m ()
-pushBack x (Deque info v) = do
-  b <- UM.unsafeRead info _dequeBackPos
-  UM.unsafeWrite v b x
-  UM.unsafeWrite info _dequeBackPos (b + 1)
+pushBack :: (U.Unbox a, PrimMonad m) => a -> Buffer (PrimState m) a -> m ()
+pushBack x Buffer{bufferVars, internalBuffer, internalBufferSize} = do
+  b <- UM.unsafeRead bufferVars _bufferBackPos
+  UM.unsafeWrite bufferVars _bufferBackPos (b + 1)
+  assert (b < internalBufferSize) $ do
+    UM.unsafeWrite internalBuffer b x
 {-# INLINE pushBack #-}
 
 pushFronts ::
-  (PrimMonad m, U.Unbox a) =>
+  (U.Unbox a, PrimMonad m) =>
   U.Vector a ->
-  Deque (PrimState m) a ->
+  Buffer (PrimState m) a ->
   m ()
-pushFronts vec (Deque info v) = do
+pushFronts vec Buffer{bufferVars, internalBuffer} = do
   let n = U.length vec
-  f <- UM.unsafeRead info _dequeFrontPos
-  UM.unsafeWrite info _dequeFrontPos (f - n)
-  U.unsafeCopy (UM.unsafeSlice (f - n) n v) vec
+  f <- UM.unsafeRead bufferVars _bufferFrontPos
+  UM.unsafeWrite bufferVars _bufferFrontPos (f - n)
+  assert (n <= f) $ do
+    U.unsafeCopy (UM.unsafeSlice (f - n) n internalBuffer) vec
 {-# INLINE pushFronts #-}
 
 pushBacks ::
-  (PrimMonad m, U.Unbox a) =>
+  (U.Unbox a, PrimMonad m) =>
   U.Vector a ->
-  Deque (PrimState m) a ->
+  Buffer (PrimState m) a ->
   m ()
-pushBacks vec (Deque info v) = do
+pushBacks vec Buffer{bufferVars, internalBuffer, internalBufferSize} = do
   let n = U.length vec
-  b <- UM.unsafeRead info _dequeBackPos
-  UM.unsafeWrite info _dequeBackPos (b + n)
-  U.unsafeCopy (UM.unsafeSlice b n v) vec
+  b <- UM.unsafeRead bufferVars _bufferBackPos
+  UM.unsafeWrite bufferVars _bufferBackPos (b + n)
+  assert (b + n - 1 < internalBufferSize) $ do
+    U.unsafeCopy (UM.unsafeSlice b n internalBuffer) vec
 {-# INLINE pushBacks #-}
-
-clearDeque :: (U.Unbox a, PrimMonad m) => Deque (PrimState m) a -> m ()
-clearDeque (Deque info v) = do
-  let o = UM.length v `quot` 2
-  UM.unsafeWrite info _dequeFrontPos o
-  UM.unsafeWrite info _dequeBackPos o
-
-freezeDeque ::
-  (PrimMonad m, U.Unbox a) =>
-  Deque (PrimState m) a ->
-  m (U.Vector a)
-freezeDeque (Deque info v) = do
-  f <- UM.unsafeRead info _dequeFrontPos
-  b <- UM.unsafeRead info _dequeBackPos
-  U.freeze $ UM.unsafeSlice f (b - f) v
