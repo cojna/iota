373c9e2e8a8dd48373695a03a7d4292719230e56
cojna <cojna@users.noreply.github.com>
2021-10-10 21:13:59 +0900

    MonoidAction prop

--- test/Data/SegTreeSpec.hs
+++ test/Data/SegTreeSpec.hs
@@ -5,9 +5,9 @@ module Data.SegTreeSpec (main, spec) where
 
 import Data.IntMod
 import Data.Monoid hiding (First (..), Last (..))
-import Data.SegTree
 import Data.Semigroup
 import Test.Prelude
+import Test.Prop.MonoidAction
 
 main :: IO ()
 main = hspec spec
@@ -32,47 +32,3 @@ spec = do
     monoidActionSpec (Proxy @(Min Int)) (Proxy @(Min Int))
   describe "MonoidAction (Max Int) (Max Int)" $ do
     monoidActionSpec (Proxy @(Max Int)) (Proxy @(Max Int))
-
-monoidActionSpec ::
-  forall f a.
-  ( Arbitrary f
-  , Monoid f
-  , Show f
-  , Arbitrary a
-  , Monoid a
-  , Show a
-  , Eq a
-  , MonoidAction f a
-  ) =>
-  Proxy f ->
-  Proxy a ->
-  Spec
-monoidActionSpec _ _ = do
-  describe "Monoid Action on Semigroup" $ do
-    prop "appMonoid mempty = id" $ prop_appMempty @f @a (Proxy @f)
-    prop "appMonoid (f <> g) x == appMonoid f (appMonoid g x)" $
-      prop_appMappend @f @a
-    prop "appMonoid f (x <> y) = appMonoid f x <> appMonoid f y" $
-      prop_monoidHomomorphismMappend @f @a
-
-prop_appMempty ::
-  forall f a.
-  (MonoidAction f a, Eq a) =>
-  Proxy f ->
-  a ->
-  Bool
-prop_appMempty _ x = appMonoid @f @a mempty x == x
-
-prop_appMappend ::
-  forall f a.
-  (MonoidAction f a, Eq a) =>
-  f ->
-  f ->
-  a ->
-  Bool
-prop_appMappend f g x = appMonoid (f <> g) x == appMonoid f (appMonoid g x)
-
-prop_monoidHomomorphismMappend ::
-  (MonoidAction f a, Monoid a, Eq a) => f -> a -> a -> Bool
-prop_monoidHomomorphismMappend f x y =
-  appMonoid f (x <> y) == appMonoid f x <> appMonoid f y
