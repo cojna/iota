f38902b1bbd8360e8b103a1fc9081f84012ba0e7
cojna <cojna@users.noreply.github.com>
2022-09-11 00:47:42 +0900

    perf: Z-Algorithm

--- src/Data/ByteString/ZAlgorithm.hs
+++ src/Data/ByteString/ZAlgorithm.hs
@@ -2,86 +2,59 @@
 
 module Data.ByteString.ZAlgorithm where
 
-import Control.Monad.Primitive
+import Control.Monad
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Unsafe as B
-import Data.Coerce (coerce)
 import Data.Function (fix)
-import qualified Data.Primitive.ByteArray as BA
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as UM
-import Foreign.Storable (sizeOf)
 
-{- | @z[i] = lcp s $ drop i s@
+{- |
 
- time complexity: O(n)
+@z[i] = lcp s $ drop i s@
 
- >>> zAlgorithm "ababab"
- [6,0,4,0,2,0]
- >>> zAlgorithm "abc$xabcxx"
- [10,0,0,0,0,3,0,0,0,0]
+time complexity: /O(n)/
+
+>>> :set -XOverloadedStrings
+>>> zAlgorithm "ababab"
+[6,0,4,0,2,0]
+>>> zAlgorithm "abc$xabcxx"
+[10,0,0,0,0,3,0,0,0,0]
+>>> zAlgorithm ""
+[]
 -}
 zAlgorithm :: B.ByteString -> U.Vector Int
-zAlgorithm bs
-  | B.null bs = U.empty
-  | otherwise = U.create $ do
-    let n = B.length bs
-    z <- UM.replicate n 0
+zAlgorithm bs = U.create $ do
+  z <- UM.unsafeNew n
+  fix
+    ( \loop !zl !zr !l -> when (l < n) $ do
+        if zr <= l
+          then do
+            let !r = lcp 0 l
+            UM.unsafeWrite z l (r - l)
+            loop l r (l + 1)
+          else do
+            zk <- UM.unsafeRead z (l - zl)
+            if zk < zr - l
+              then do
+                UM.unsafeWrite z l zk
+                loop zl zr (l + 1)
+              else do
+                let !r = lcp (zr - l) zr
+                UM.unsafeWrite z l (r - l)
+                loop l r (l + 1)
+    )
+    0
+    1
+    1
+  when (n > 0) $ do
     UM.write z 0 n
-    zbox <- newZBox
-    U.forM_ (U.tail $ U.generate n id) $ \i -> do
-      r <- readR zbox
-      if i > r
-        then do
-          writeL zbox i
-          writeR zbox i
-          extendR bs zbox >>= UM.unsafeWrite z i
-        else do
-          l <- readL zbox
-          let k = i - l
-          zk <- UM.unsafeRead z k
-          if zk < r - i + 1
-            then do
-              UM.unsafeWrite z i zk
-            else do
-              writeL zbox i
-              extendR bs zbox >>= UM.unsafeWrite z i
-    return z
+  return z
+  where
+    !n = B.length bs
 
-extendR :: (PrimMonad m) => B.ByteString -> ZBox (PrimState m) -> m Int
-extendR bs zbox = do
-  l <- readL zbox
-  r <- readR zbox
-  let !n = B.length bs
-  let r' = flip fix r $ \loop !i ->
-        if i < n && B.unsafeIndex bs (i - l) == B.unsafeIndex bs i
-          then loop (i + 1)
-          else i
-  writeR zbox (r' - 1)
-  return $! r' - l
-{-# INLINE extendR #-}
-
-newtype ZBox s = ZBox {unZBox :: BA.MutableByteArray s}
-
-newZBox :: (PrimMonad m) => m (ZBox (PrimState m))
-newZBox = do
-  zbox <- ZBox <$> BA.newByteArray (sizeOf (undefined :: Int) * 2)
-  writeL zbox 0
-  writeR zbox 0
-  return zbox
-
-readL :: (PrimMonad m) => ZBox (PrimState m) -> m Int
-readL zbox = BA.readByteArray (coerce zbox) 0
-{-# INLINE readL #-}
-
-readR :: (PrimMonad m) => ZBox (PrimState m) -> m Int
-readR zbox = BA.readByteArray (coerce zbox) 1
-{-# INLINE readR #-}
-
-writeL :: (PrimMonad m) => ZBox (PrimState m) -> Int -> m ()
-writeL zbox = BA.writeByteArray (coerce zbox) 0
-{-# INLINE writeL #-}
-
-writeR :: (PrimMonad m) => ZBox (PrimState m) -> Int -> m ()
-writeR zbox = BA.writeByteArray (coerce zbox) 1
-{-# INLINE writeR #-}
+    lcp :: Int -> Int -> Int
+    lcp = fix $ \loop !l !r ->
+      if r < n && B.unsafeIndex bs l == B.unsafeIndex bs r
+        then loop (l + 1) (r + 1)
+        else r
