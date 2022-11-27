c169ee87ad48b775da70f812461e63c25baaf4f5
cojna <cojna@users.noreply.github.com>
2022-11-27 18:40:12 +0900

    style: fourmolu

--- src/Data/Vector/Utils.hs
+++ src/Data/Vector/Utils.hs
@@ -15,16 +15,19 @@ group = groupBy (==)
 groupBy :: (G.Vector u a, G.Vector v (u a)) => (a -> a -> Bool) -> u a -> v (u a)
 groupBy eq = G.unfoldr $ \u ->
   if G.null u
-  then Nothing
-  else case G.unsafeHead u of
-    u0 -> Just $ G.span (eq u0) u
+    then Nothing
+    else case G.unsafeHead u of
+      u0 -> Just $ G.span (eq u0) u
 
 tuples2 :: (G.Vector v a, G.Vector v (a, a)) => v a -> v (a, a)
 tuples2 = G.unfoldr $ \v ->
-  if G.length v >= 2 then (
-    let !x = G.unsafeIndex v 0
-        !y = G.unsafeIndex v 1
-     in Just ((x, y), G.drop 2 v)) else Nothing
+  if G.length v >= 2
+    then
+      ( let !x = G.unsafeIndex v 0
+            !y = G.unsafeIndex v 1
+         in Just ((x, y), G.drop 2 v)
+      )
+    else Nothing
 
 tuples2N :: (G.Vector v a, G.Vector v (a, a)) => Int -> v a -> v (a, a)
 tuples2N n = G.unfoldrN n $ \v ->
@@ -34,11 +37,14 @@ tuples2N n = G.unfoldrN n $ \v ->
 
 tuples3 :: (G.Vector v a, G.Vector v (a, a, a)) => v a -> v (a, a, a)
 tuples3 = G.unfoldr $ \v ->
-  if G.length v >= 3 then (
-    let !x = G.unsafeIndex v 0
-        !y = G.unsafeIndex v 1
-        !z = G.unsafeIndex v 2
-     in Just ((x, y, z), G.drop 3 v)) else Nothing
+  if G.length v >= 3
+    then
+      ( let !x = G.unsafeIndex v 0
+            !y = G.unsafeIndex v 1
+            !z = G.unsafeIndex v 2
+         in Just ((x, y, z), G.drop 3 v)
+      )
+    else Nothing
 
 tuples3N :: (G.Vector v a, G.Vector v (a, a, a)) => Int -> v a -> v (a, a, a)
 tuples3N n = G.unfoldrN n $ \v ->
@@ -49,12 +55,15 @@ tuples3N n = G.unfoldrN n $ \v ->
 
 tuples4 :: (G.Vector v a, G.Vector v (a, a, a, a)) => v a -> v (a, a, a, a)
 tuples4 = G.unfoldr $ \v ->
-  if G.length v >= 3 then (
-    let !x = G.unsafeIndex v 0
-        !y = G.unsafeIndex v 1
-        !z = G.unsafeIndex v 2
-        !w = G.unsafeIndex v 3
-     in Just ((x, y, z, w), G.drop 4 v)) else Nothing
+  if G.length v >= 3
+    then
+      ( let !x = G.unsafeIndex v 0
+            !y = G.unsafeIndex v 1
+            !z = G.unsafeIndex v 2
+            !w = G.unsafeIndex v 3
+         in Just ((x, y, z, w), G.drop 4 v)
+      )
+    else Nothing
 
 tuples4N ::
   (G.Vector v a, G.Vector v (a, a, a, a)) =>
