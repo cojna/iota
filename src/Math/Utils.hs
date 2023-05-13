4e3b35f76349b694b0432ddcbc7945a848f4f02a
cojna <cojna@users.noreply.github.com>
2023-05-13 17:03:09 +0900

    fix: floorSqrt for large integers

--- src/Math/Utils.hs
+++ src/Math/Utils.hs
@@ -1,11 +1,37 @@
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Math.Utils where
 
 import Data.Bits
 
+{- |
+>>> floorSqrt 0
+0
+>>> floorSqrt 1
+1
+>>> floorSqrt 2
+1
+>>> floorSqrt 4
+2
+>>> floorSqrt (12345 * 12345)
+12345
+>>> floorSqrt (2^52 + 2^27)
+67108864
+>>> floorSqrt maxBound
+3037000499
+-}
 floorSqrt :: Int -> Int
-floorSqrt = floor . sqrt @Double . fromIntegral
+floorSqrt n
+  | n <= 1 = n
+  | otherwise =
+    let !k = 32 - unsafeShiftR (countLeadingZeros (n - 1)) 1
+        !x0 = unsafeShiftL 1 k
+        !x1 = unsafeShiftR (x0 + unsafeShiftR n k) 1
+     in go x0 x1
+  where
+    go !x !x'
+      | x' < x = go x' (unsafeShiftR (x' + quot n x') 1)
+      | otherwise = x
 
 {- |
 BSR (Bit Scan Reverse)
--- test/Math/UtilsSpec.hs
+++ test/Math/UtilsSpec.hs
@@ -12,6 +12,15 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "floorSqrt" $ do
+    it "floorSqrt 0 = 0" $ do
+      floorSqrt 0 `shouldBe` 0
+    it "floorSqrt 1 = 1" $ do
+      floorSqrt 1 `shouldBe` 1
+    it "floorSqrt (2^52+2^27) = 67108864" $ do
+      floorSqrt (2 ^ (52 :: Int) + 2 ^ (27 :: Int))
+        `shouldBe` 67108864
+    it "floorSqrt maxBound = 3037000499" $ do
+      floorSqrt maxBound `shouldBe` 3037000499
     prop "floor (sqrt x)" prop_floorSqrt
   describe "floorLog2" $ do
     it "floorLog2 1 = 0" $ do
@@ -29,9 +38,11 @@ spec = do
     prop "2 ^ n <= floorLog2 n < 2 ^ (n + 1)" prop_floorLog2
 
 prop_floorSqrt :: NonNegative Int -> Bool
-prop_floorSqrt (getNonNegative -> n) = res * res <= n && (res + 1) * (res + 1) > n
+prop_floorSqrt (getNonNegative -> n) =
+  res * res <= fromIntegral n
+    && fromIntegral n < (res + 1) * (res + 1)
   where
-    res = floorSqrt n
+    res = toInteger $ floorSqrt n
 
 prop_floorLog2 :: Positive Int -> Bool
 prop_floorLog2 (getPositive -> n) =
