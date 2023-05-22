dc41c3abe447bbaf6c9f98ee3e96fd36840cf98f
cojna <cojna@users.noreply.github.com>
2023-05-22 21:17:42 +0900

    docs: 2-SAT

--- src/Algorithm/TwoSat.hs
+++ src/Algorithm/TwoSat.hs
@@ -10,18 +10,35 @@ import qualified Data.Vector.Unboxed as U
 import Data.Graph.Sparse
 import Data.Graph.Sparse.SCC
 
+{- | 2-SAT
+
+@(a0 \\\/ a1) \/\\ not a0@
+
+>>> twoSat 2 2 (\b -> addClauseCNF b (0, True) (1, True) >> addClauseCNF b (0, False) (0, False))
+Just [False,True]
+
+@a0 \/\\ not a1 \/\\ (not a0 \\\/ a1)@
+
+>>> print $ twoSat 2 3 (\b -> addClauseCNF b (0, True) (0, True) >> addClauseCNF b (1, False) (1, False) >> addClauseCNF b (0, False) (1, True))
+Nothing
+
+@a0 \\\/ not a0@
+
+>>> twoSat 1 1 (\b -> addClauseCNF b (0, True) (0, False))
+Just [True]
+-}
 twoSat ::
   -- | the number of variables
   Int ->
-  -- | the number of clauses
+  -- | upper bound on the number of clauses
   Int ->
   -- | CNF(Conjunctive Normal Form)
   (forall s. CNFBuilder s () -> ST s ()) ->
   Maybe (U.Vector Bool)
 twoSat numVars numClauses run
   | satisfiable = Just $
-    U.generate numVars $ \i ->
-      U.unsafeIndex comp i > U.unsafeIndex comp (i + offset)
+      U.generate numVars $ \i ->
+        U.unsafeIndex comp i > U.unsafeIndex comp (i + offset)
   | otherwise = Nothing
   where
     satisfiable = U.and $
