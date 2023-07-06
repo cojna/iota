717ea0cf19f9a6c924329f23e4a93ac235a4dc89
cojna <cojna@users.noreply.github.com>
2023-07-07 01:07:15 +0900

    style: fourmolu

--- src/Data/Graph/Dense/Prim.hs
+++ src/Data/Graph/Dense/Prim.hs
@@ -23,40 +23,40 @@ primDense ::
   U.Vector Int
 primDense n root gr
   | root >= n || U.length gr /= n * n =
-    error "primDense: Invalid Arguments"
+      error "primDense: Invalid Arguments"
   | otherwise = U.create $ do
-    let !inf = 2 * U.maximum gr
-    parent <- UM.replicate n (-1)
-    dist <- UM.new n
-    used <- UM.replicate n False
-    UM.write dist root 0
-    UM.write used root True
-    rep n $ \i -> do
-      when (i /= root) $ do
-        UM.write parent i root
-      UM.write dist i $ U.unsafeIndex gr (root * n + i)
-    rep (n - 1) $ \_ -> do
-      v <-
-        fmap snd $
-          MS.foldM'
-            ( \acc i -> do
-                UM.unsafeRead used i >>= \case
-                  False -> do
-                    d <- UM.unsafeRead dist i
-                    return $! min acc (d, i)
-                  True -> return acc
-            )
-            (inf, -1)
+      let !inf = 2 * U.maximum gr
+      parent <- UM.replicate n (-1)
+      dist <- UM.new n
+      used <- UM.replicate n False
+      UM.write dist root 0
+      UM.write used root True
+      rep n $ \i -> do
+        when (i /= root) $ do
+          UM.write parent i root
+        UM.write dist i $ U.unsafeIndex gr (root * n + i)
+      rep (n - 1) $ \_ -> do
+        v <-
+          fmap snd
+            $ MS.foldM'
+              ( \acc i -> do
+                  UM.unsafeRead used i >>= \case
+                    False -> do
+                      d <- UM.unsafeRead dist i
+                      return $! min acc (d, i)
+                    True -> return acc
+              )
+              (inf, -1)
             $ stream 0 n
-      UM.write used v True
-      rep n $ \u -> do
-        UM.unsafeRead used u >>= \case
-          False -> do
-            du <- UM.unsafeRead dist u
-            let dvu = U.unsafeIndex gr (v * n + u)
-            when (dvu < du) $ do
-              UM.unsafeWrite dist u dvu
-              UM.unsafeWrite parent u v
-          True -> return ()
-    return parent
+        UM.write used v True
+        rep n $ \u -> do
+          UM.unsafeRead used u >>= \case
+            False -> do
+              du <- UM.unsafeRead dist u
+              let dvu = U.unsafeIndex gr (v * n + u)
+              when (dvu < du) $ do
+                UM.unsafeWrite dist u dvu
+                UM.unsafeWrite parent u v
+            True -> return ()
+      return parent
 {-# INLINE primDense #-}
