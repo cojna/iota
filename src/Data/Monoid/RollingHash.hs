302ae6d3f9b7bac99675ada3baa43061057d9cdd
cojna <cojna@users.noreply.github.com>
2023-12-03 04:52:52 +0900

    feat: RollingHash Monoid

---
+++ src/Data/Monoid/RollingHash.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Data.Monoid.RollingHash where
+
+import Data.Char
+import Data.Coerce
+import qualified Data.Foldable as F
+import Data.Monoid
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Generic.Mutable as GM
+import qualified Data.Vector.Unboxed as U
+import GHC.Exts
+import GHC.TypeLits
+
+import Data.Monoid.Affine
+import Data.RollingHash
+
+{- |
+>>> :set -XDataKinds -XOverloadedStrings
+>>> runRollingHashBuilder @2047 $ "abc" <> "def"
+1182147938584434692
+>>> runRollingHashBuilder @2047 "abcdef"
+1182147938584434692
+>>> mempty @(RollingHashBuilder 2047)
+Affine {getAffine1 = 1, getAffine0 = 0}
+-}
+newtype RollingHashBuilder b = RHB (Affine (RollingHash b))
+  deriving newtype (Eq, Show)
+  deriving (Semigroup, Monoid) via Dual (Affine (RollingHash b))
+
+instance (KnownNat b) => IsString (RollingHashBuilder b) where
+  fromString = F.foldMap' (singletonRHB . ord)
+
+newtype instance U.MVector s (RollingHashBuilder b) = MV_RollingHashBuilder (U.MVector s Int)
+newtype instance U.Vector (RollingHashBuilder b) = V_RollingHashBuilder (U.Vector Int)
+deriving newtype instance GM.MVector U.MVector (RollingHashBuilder b)
+deriving newtype instance G.Vector U.Vector (RollingHashBuilder b)
+instance U.Unbox (RollingHashBuilder b)
+
+runRollingHashBuilder :: RollingHashBuilder b -> RollingHash b
+runRollingHashBuilder (RHB (Affine _ h)) = h
+
+{- |
+>>> :set -XDataKinds
+>>> singletonRHB @2047 123
+Affine {getAffine1 = 2047, getAffine0 = 123}
+-}
+singletonRHB :: forall b. (KnownNat b) => Int -> RollingHashBuilder b
+singletonRHB x = coerce (Affine (RollingHash b) (RollingHash x))
+  where
+    b = fromIntegral (natVal' @b proxy#)
