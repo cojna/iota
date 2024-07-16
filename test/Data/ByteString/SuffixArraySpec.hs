7295542109e90d68b23d5cbe5465cfc32911d404
cojna <cojna@users.noreply.github.com>
2024-07-17 03:06:10 +0900

    feat: findSubstrings

--- test/Data/ByteString/SuffixArraySpec.hs
+++ test/Data/ByteString/SuffixArraySpec.hs
@@ -20,6 +20,18 @@ spec = do
     prop "naive" prop_naive
     it "buildSuffixArray \"\" == [0]" $ do
       buildSuffixArray B.empty `shouldBe` SuffixArray (U.singleton 0)
+  describe "findSubstringsSA" $ do
+    describe "abababa" $ do
+      let bs = "abababa"
+          sa = buildSuffixArray bs
+      it "find ab" $ do
+        findSubstringsSA bs sa "ab" `shouldBe` U.fromList [4, 2, 0]
+      it "find xxx" $ do
+        findSubstringsSA bs sa "xxx" `shouldBe` U.empty
+      it "find \"\"" $ do
+        findSubstringsSA bs sa "" `shouldBe` getSuffixArray sa
+      it "find abababa" $ do
+        findSubstringsSA bs sa "abababa" `shouldBe` U.singleton 0
 
 prop_naive :: ByteStringOf "ab" -> Bool
 prop_naive (getByteStringOf -> bs) =
