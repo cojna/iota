a3d9b5679ee026449a967479983dabf8281742ec
cojna <cojna@users.noreply.github.com>
2025-01-21 02:53:48 +0900

    feat: MonoidAction instances

--- test/Data/Monoid/ActionSpec.hs
+++ test/Data/Monoid/ActionSpec.hs
@@ -1,7 +1,7 @@
 module Data.Monoid.ActionSpec (main, spec) where
 
 import Data.IntMod
-import Data.Monoid hiding (First (..), Last (..))
+import Data.Semigroup
 import Test.Prelude
 import Test.Prop.MonoidAction
 
@@ -12,3 +12,13 @@ spec :: Spec
 spec = do
   describe "MonoidAction () (Product IntMod)" $ do
     monoidActionSpec (Proxy @()) (Proxy @(Product IntMod))
+  describe "MonoidAction (Sum Int) Int" $ do
+    monoidActionSpec (Proxy @(Sum Int)) (Proxy @Int)
+  describe "MonoidAction (Product Int) Int" $ do
+    monoidActionSpec (Proxy @(Product Int)) (Proxy @Int)
+  describe "MonoidAction (Product Int) (Sum Int)" $ do
+    monoidActionSpec (Proxy @(Product Int)) (Proxy @(Sum Int))
+  describe "MonoidAction (Max Int) Int" $ do
+    monoidActionSpec (Proxy @(Max Int)) (Proxy @Int)
+  describe "MonoidAction (Min Int) Int" $ do
+    monoidActionSpec (Proxy @(Min Int)) (Proxy @Int)
