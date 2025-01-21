bfa817a62dc10bdcc10904fa9211d4d8731272be
cojna <cojna@users.noreply.github.com>
2025-01-22 01:19:20 +0900

    feat: binary search on Sum Fenwick Tree

--- src/Data/FenwickTree/Sum.hs
+++ src/Data/FenwickTree/Sum.hs
@@ -7,6 +7,8 @@ import Data.Function
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as UM
 
+import My.Prelude (floorPowerOf2)
+
 newtype SumFenwickTree s a = SumFenwickTree {getSumFenwickTree :: UM.MVector s a}
 
 newSumFenwickTree ::
@@ -110,3 +112,85 @@ writeSFT ::
   m ()
 writeSFT ft i x = readSFT ft i >>= addAt ft i . (x -)
 {-# INLINE writeSFT #-}
+
+{- |
+min i s.t. sum [0..i) >= w
+
+>>> ft <- buildSumFenwickTree @Int (U.fromList [1,1,1,1,1])
+>>> lowerBoundSFT ft 3
+3
+>>> sumTo ft 3
+3
+>>> lowerBoundSFT ft 0
+0
+>>> lowerBoundSFT ft 1
+1
+>>> lowerBoundSFT ft 10
+6
+
+>>> ft <- buildSumFenwickTree @Int (U.fromList [1,1,0,0,0])
+>>> lowerBoundSFT ft 2
+2
+-}
+lowerBoundSFT ::
+  (U.Unbox a, Num a, Ord a, PrimMonad m) =>
+  SumFenwickTree (PrimState m) a ->
+  a ->
+  m Int
+lowerBoundSFT (SumFenwickTree ft) s0
+  | s0 <= 0 = return 0
+  | otherwise = go s0 (floorPowerOf2 n) 0
+  where
+    !n = UM.length ft
+    go !s !w !i
+      | w == 0 = return $! i + 1
+      | otherwise = do
+          if i + w < n
+            then do
+              fiw <- UM.unsafeRead ft (i + w)
+              if fiw < s
+                then go (s - fiw) (w !>>. 1) (i + w)
+                else go s (w !>>. 1) i
+            else go s (w !>>. 1) i
+{-# INLINE lowerBoundSFT #-}
+
+{- |
+max i s.t. sum [0..i) <= w
+
+>>> ft <- buildSumFenwickTree @Int (U.fromList [1,1,1,1,1])
+>>> upperBoundSFT ft 3
+3
+>>> sumTo ft 3
+3
+>>> upperBoundSFT ft 0
+0
+>>> upperBoundSFT ft 1
+1
+>>> upperBoundSFT ft 10
+5
+
+>>> ft <- buildSumFenwickTree @Int (U.fromList [1,1,0,0,1])
+>>> upperBoundSFT ft 2
+4
+-}
+upperBoundSFT ::
+  (U.Unbox a, Num a, Ord a, PrimMonad m) =>
+  SumFenwickTree (PrimState m) a ->
+  a ->
+  m Int
+upperBoundSFT (SumFenwickTree ft) s0
+  | s0 < 0 = return 0
+  | otherwise = go s0 (floorPowerOf2 n) 0
+  where
+    !n = UM.length ft
+    go !s !w !i
+      | w == 0 = return i
+      | otherwise = do
+          if i + w < n
+            then do
+              fiw <- UM.unsafeRead ft (i + w)
+              if fiw <= s
+                then go (s - fiw) (w !>>. 1) (i + w)
+                else go s (w !>>. 1) i
+            else go s (w !>>. 1) i
+{-# INLINE upperBoundSFT #-}
